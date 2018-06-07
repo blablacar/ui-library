aefd1b8187f9172b84bd01f0ce4f267a3e9ac799
Baptiste Gios <baptiste.gios@blablacar.com>
2018-06-07 15:24:41 +0200

    Remove story test

--- src/autoComplete/story.tsx
+++ src/autoComplete/story.tsx
@@ -59,7 +59,6 @@ class AutoCompleteExample extends Component<AutoCompleteExampleProps, AutoComple
           name="city"
           placeholder="Search here…"
           defaultValue={text('defaultValue')}
-          bodyClassName="toto"
           isSearching={this.state.isSearching}
           searchForItems={this.searchForItems}
           items={this.state.items}
