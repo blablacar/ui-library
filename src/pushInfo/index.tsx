6fe1f6545207002ab3cf22f961c16549c9c44ab3
Perrine Boissières <perrine.boissieres@gmail.com>
2018-06-07 14:43:03 +0200

    Add callback to PushInfos animation

--- src/pushInfo/index.tsx
+++ src/pushInfo/index.tsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import cc from 'classcat'
 
 import prefix from '_utils'
-import style from 'pushInfo/style'
+import style, { animationDuration, animationDelay } from 'pushInfo/style'
 
 interface PushInfoProps {
   readonly className?: Classcat.Class
   readonly icon?: React.ReactNode
   readonly headline: string
   readonly content?: string
+  readonly onAnimationEnd?: Function
 }
 
-const PushInfo = ({ className, icon, headline, content }: PushInfoProps) => (
-  <div className={cc(['kirk-pushInfo', className])}>
-    {icon && (
-      <div className="kirk-pushInfo-icon">{icon}</div>
-    )
+class PushInfo extends Component<PushInfoProps> {
+  componentDidMount() {
+    if (this.props.onAnimationEnd) {
+      setTimeout(this.props.onAnimationEnd, animationDuration + animationDelay)
     }
-    <div>
-      <h2 className={cc([
-        'kirk-pushInfo-headline',
-        { 'kirk-pushInfo-headline--standalone': !content },
-      ])}>
-        {headline}
-      </h2>
-      {content && <p className="kirk-pushInfo-content">{content}</p>}
-    </div>
-    <style jsx>{style}</style>
-  </div>
-)
+  }
+  render() {
+    const { className, icon, headline, content } = this.props
+    return (
+      <div className={cc(['kirk-pushInfo', className])}>
+        {icon && (
+          <div className="kirk-pushInfo-icon">{icon}</div>
+        )
+        }
+        <div>
+          <h2 className={cc([
+            'kirk-pushInfo-headline',
+            { 'kirk-pushInfo-headline--standalone': !content },
+          ])}>
+            {headline}
+          </h2>
+          {content && <p className="kirk-pushInfo-content">{content}</p>}
+        </div>
+        <style jsx>{style}</style>
+      </div>
+    )
+  }
+}
 
 export default PushInfo
-
