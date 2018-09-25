bfa4c4c65ffab88e332eb9a09dfb3de66061dbf9
Jerome Smadja <wakooka@users.noreply.github.com>
2018-09-25 16:07:30 +0200

    changed drawer transition (#87)

--- src/drawer/style.ts
+++ src/drawer/style.ts
@@ -37,25 +37,24 @@ export default css`
 
   .kirk-drawer-scrollableContent {
     position: absolute;
-    z-index: 1050;
+    z-index: 2;
     top: 0;
     left: 0;
-    bottom: 0;
     overflow-y: auto;
     background-color: ${color.white};
     transition: transform ${transition.duration.base} ease-in-out;
     will-change: transform;
-    transform: translateX(-100%);
+    transform: translateY(-100%);
     max-width: 100%;
     -webkit-overflow-scrolling: touch;
   }
 
   .kirk-drawer--open .kirk-drawer-scrollableContent {
     box-shadow: 0 36px 36px 0 rgba(0, 0, 0, 0.3);
-    transform: translateX(0%);
+    transform: translateY(0%);
   }
 
   .kirk-drawer--close .kirk-drawer-scrollableContent {
-    transform: translateX(-100%);
+    transform: translateY(-100%);
   }
 `
