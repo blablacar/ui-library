b7ff0378ab32232df366fa61b94e2e28b1daf24f
Baptiste Gios <baptiste.gios@blablacar.com>
2018-10-10 22:29:13 +0200

    Refacto with Text component + class renaming

--- src/itinerary/style.ts
+++ src/itinerary/style.ts
@@ -12,22 +12,18 @@ export default css`
 
   .kirk-itinerary-headline {
     padding-bottom: ${space.l};
-    font-size: ${font.m.size};
-    line-height: ${font.m.lineHeight};
-    font-weight: ${fontWeight.regular};
-    color: ${color.primaryText};
   }
 
   .kirk-itinerary-location {
     display: flex;
-    font-size: ${font.m.size};
-    line-height: ${font.m.lineHeight};
-    font-weight: ${fontWeight.medium};
-    color: ${color.primaryText};
   }
 
   .kirk-itinerary-location time {
     min-width: ${timeWidth};
+    font-size: ${font.m.size};
+    line-height: ${font.m.lineHeight};
+    font-weight: ${fontWeight.medium};
+    color: ${color.primaryText};
   }
 
   .kirk-itinerary-location div {
@@ -86,93 +82,84 @@ export default css`
     height: 6px;
   }
 
-  .kirk-itinerary-subtext {
-    display: block;
-    font-size: ${font.s.size};
-    line-height: ${font.s.lineHeight};
-    font-weight: ${fontWeight.regular};
-    color: ${color.secondaryText};
-  }
-
-  .kirk-itinerary-fromDeparture,
-  .kirk-itinerary-fromArrival {
+  .kirk-itinerary-fromAddon,
+  .kirk-itinerary-toAddon {
     height: ${distanceFromHeight};
   }
 
-  .kirk-itinerary-fromDeparture::before,
-  .kirk-itinerary-fromDeparture::after,
-  .kirk-itinerary-location.kirk-itinerary-location--fromArrival::before,
-  .kirk-itinerary-location.kirk-itinerary-location--fromArrival::after {
+  .kirk-itinerary-fromAddon::before,
+  .kirk-itinerary-fromAddon::after,
+  .kirk-itinerary-location.kirk-itinerary-location--toAddon::before,
+  .kirk-itinerary-location.kirk-itinerary-location--toAddon::after {
     content: '';
     display: block;
     position: absolute;
   }
 
-  .kirk-itinerary-fromDeparture::before,
-  .kirk-itinerary-location.kirk-itinerary-location--fromArrival::before {
+  .kirk-itinerary-fromAddon::before,
+  .kirk-itinerary-location.kirk-itinerary-location--toAddon::before {
     width: 0;
-    border: 1px dashed #708c91;
-    left: calc(${timeWidth} + 1px);
+    border: 2px solid ${color.lightBackground};
+    left: ${timeWidth};
+    bottom: -4px;
   }
 
-  .kirk-itinerary--small .kirk-itinerary-fromDeparture::before,
-  .kirk-itinerary--small .kirk-itinerary-location.kirk-itinerary-location--fromArrival::before {
-    left: 3px;
+  .kirk-itinerary--small .kirk-itinerary-fromAddon::before,
+  .kirk-itinerary--small .kirk-itinerary-location.kirk-itinerary-location--toAddon::before {
+    left: 2px;
   }
 
-  .kirk-itinerary-fromDeparture::before {
+  .kirk-itinerary-fromAddon::before {
     height: 100%;
   }
 
-  .kirk-itinerary-location.kirk-itinerary-location--fromArrival::before {
+  .kirk-itinerary-location.kirk-itinerary-location--toAddon::before {
     height: calc(100% + ${distanceFromHeight});
     bottom: -${distanceFromHeight};
   }
 
-  .kirk-itinerary-fromDeparture::after,
-  .kirk-itinerary-location.kirk-itinerary-location--fromArrival::after {
-    width: 6px;
-    height: 6px;
+  .kirk-itinerary-fromAddon::after,
+  .kirk-itinerary-location.kirk-itinerary-location--toAddon::after {
+    width: 8px;
+    height: 8px;
     background-color: #fff;
-    border: 1px solid ${color.secondaryText};
+    border: 2px solid ${color.lightBackground};
     border-radius: 50%;
-    left: calc(${timeWidth} - 1px);
+    left: calc(${timeWidth} - 2px);
   }
 
-  .kirk-itinerary--small .kirk-itinerary-fromDeparture::after,
-  .kirk-itinerary--small .kirk-itinerary-location.kirk-itinerary-location--fromArrival::after {
-    left: 1px;
+  .kirk-itinerary--small .kirk-itinerary-fromAddon::after,
+  .kirk-itinerary--small .kirk-itinerary-location.kirk-itinerary-location--toAddon::after {
+    left: 0;
   }
 
-  .kirk-itinerary-fromDeparture::after {
+  .kirk-itinerary-fromAddon::after {
     top: 0;
   }
 
-  .kirk-itinerary-location.kirk-itinerary-location--fromArrival::after {
+  .kirk-itinerary-location.kirk-itinerary-location--toAddon::after {
     bottom: -${distanceFromHeight};
   }
 
-  .kirk-itinerary-fromDeparture span,
-  .kirk-itinerary-fromArrival span {
+  :global(.kirk-itinerary-fromAddon span),
+  :global(.kirk-itinerary-toAddon span) {
     display: inline-block;
     padding-left: ${space.l};
-    font-size: ${font.s.size};
-    line-height: ${font.s.lineHeight};
-    color: ${color.secondaryText};
+    color: ${color.lightBackground};
     position: absolute;
     left: ${timeWidth};
   }
 
-  .kirk-itinerary--small .kirk-itinerary-fromDeparture span,
-  .kirk-itinerary--small .kirk-itinerary-fromArrival span {
+  :global(.kirk-itinerary--small .kirk-itinerary-fromAddon span),
+    :global(.kirk-itinerary--small .kirk-itinerary-toAddon span) {
     left: 0;
   }
 
-  .kirk-itinerary-fromDeparture span {
-    top: 8px;
+  :global(.kirk-itinerary-fromAddon span) {
+    top: -4px;
   }
 
-  .kirk-itinerary-fromArrival span {
-    bottom: 8px;
+  :global(.kirk-itinerary-toAddon span) {
+    bottom: -4px;
   }
 `
