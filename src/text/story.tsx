a5fb9fefa6d84a6f9609db9987a5f6e869e4b531
Cyrille Jouineau <cyrille.jouineau@blablacar.com>
2018-12-10 16:02:32 +0100

    remove colors, use color

--- src/text/story.tsx
+++ src/text/story.tsx
@@ -10,21 +10,13 @@ import Text, { TextTagType, TextDisplayType } from 'text'
 const stories = storiesOf('Text', module)
 stories.addDecorator(withKnobs)
 
-const colors = Object.keys(color).reduce(
-  (acc, key) => ({
-    ...acc,
-    [key]: color[key],
-  }),
-  {},
-)
-
 stories.add(
   'basic',
   withInfo('')(() => (
     <Text
       display={selectV2('Display', TextDisplayType, TextDisplayType.BODY)}
       tag={selectV2('Tag', TextTagType, TextTagType.SPAN)}
-      textColor={selectV2('Color', colors, 'primary')}
+      textColor={selectV2('Color', color, 'primary')}
       newlineToBr={boolean('newlineToBr', true)}
     >
       {text('Text', 'This is an example')}
