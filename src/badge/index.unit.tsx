b94c1669352e179a9cc45af29df6e4430e1f13a1
Cyrille Jouineau <cyrille.jouineau@blablacar.com>
2019-07-22 18:29:56 +0200

    update tabs (isWrapped, tablistWrapperClassName, selected highlight, extend click area, Badge position)

--- src/badge/index.unit.tsx
+++ src/badge/index.unit.tsx
@@ -6,23 +6,23 @@ import { CheckIcon } from 'icon/checkIcon'
 
 it('Should read the aria-label attribute and not the children if both are provided', () => {
   const wrapper = shallow(<Badge ariaLabel="Unread messages">12</Badge>)
-  expect(wrapper.find('span').prop('aria-hidden')).toBe(true)
+  expect(wrapper.find('span > span').prop('aria-hidden')).toBe(true)
 })
 
 it('Should display the children if this is a number', () => {
   const value = 12
   const wrapper = shallow(<Badge>value</Badge>)
-  expect(wrapper.find('span').prop('aria-hidden')).toBe(false)
+  expect(wrapper.find('span > span').prop('aria-hidden')).toBe(false)
 })
 
 it('Should read the children if aria-label is empty', () => {
   const wrapper = shallow(<Badge>12</Badge>)
-  expect(wrapper.find('span').text()).toBe('12')
+  expect(wrapper.find('span > span').text()).toBe('12')
 })
 
 it('Should not render if children is empty', () => {
   const wrapper = shallow(<Badge />)
-  expect(wrapper.find('span').exists()).toBe(false)
+  expect(wrapper.find('span > span').exists()).toBe(false)
 })
 
 it('can accept a JSX.Element as children', () => {
