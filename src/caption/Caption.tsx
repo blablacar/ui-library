49ba4935008d7f0612a79f476f0d2588d81a5a17
Pierre T <pierre.thirouin@blablacar.com>
2019-07-29 16:24:32 +0300

    Migrate Caption to styled-component (#311)

--- src/caption/Caption.tsx
+++ src/caption/Caption.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import cc from 'classcat'
 
 import Button from 'button'
-import style from 'caption/style'
 
 export const renderSecondary = (href?: string, secondaryText?: string) =>
   href ? (
@@ -15,32 +14,25 @@ export const renderSecondary = (href?: string, secondaryText?: string) =>
 
 interface Caption {
   readonly className?: Classcat.Class
+  readonly extraClassName?: Classcat.Class
   readonly children: any
   readonly isoDate: string
   readonly href?: string
   readonly secondaryText?: string
 }
 
-const Caption = ({ className, children, href, secondaryText, isoDate }: Caption) => (
-  <div className={cc(['kirk-caption', className])}>
+const Caption = ({
+  className,
+  extraClassName,
+  children,
+  href,
+  secondaryText,
+  isoDate,
+}: Caption) => (
+  <div className={cc(['kirk-caption', className, extraClassName])}>
     <time dateTime={isoDate || null}>{children}</time>
     {secondaryText && <span> - {renderSecondary(href, secondaryText)}</span>}
-    <style jsx>{style}</style>
   </div>
 )
 
 export default Caption
-
-// @TODO
-
-// isoDate(props, propName, componentName) {
-//   if (props[propName]) {
-//     const validDate = new Date(props[propName]).getTime()
-//     if (isNaN(validDate)) {
-//       return new Error(
-//         `Invalid prop ${propName} supplied to ${componentName}. Not a valid ISO date.`,
-//       )
-//     }
-//   }
-//   return null
-// },
