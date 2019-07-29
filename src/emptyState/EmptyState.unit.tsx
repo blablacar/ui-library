b56d0ee8e6dc800493bedd8786c4d027efe5b46c
Vincent Renaudin <vincent.renaudin@blablacar.com>
2019-07-29 15:42:09 +0200

    [css-in-js] EmptyState widget

--- src/emptyState/EmptyState.unit.tsx
+++ src/emptyState/EmptyState.unit.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import EmptyState from 'emptyState'
+import EmptyState from './EmptyState'
 import Title from 'title'
 import Button from 'button'
 
