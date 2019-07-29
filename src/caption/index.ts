602c07d4c1e28d9af02d844700e5b5b912e37ff0
Pierre Thirouin <pierre.thirouin@blablacar.com>
2019-07-29 14:47:17 +0200

    Migrate Caption to styled-component

--- src/caption/index.ts
+++ src/caption/index.ts
@@ -1,8 +1,10 @@
+import styled from 'styled-components'
 import css from 'styled-jsx/css'
 import { color, font } from '_utils/branding'
+import Caption from './Caption'
 
-export default css`
-  .kirk-caption {
+const StyledCaption = styled(Caption)`
+  & {
     font-size: ${font.s.size};
     line-height: ${font.s.lineHeight};
     color: ${color.secondaryText};
@@ -13,3 +15,5 @@ export default css`
     color: ${color.secondaryText};
   }
 `
+
+export default StyledCaption
