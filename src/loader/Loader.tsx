1f160c59125775c68eb9a395231115bf64c68799
Vincent Renaudin <vincent.renaudin@blablacar.com>
2019-07-29 14:22:43 +0200

    css-in-js loader icon

---
+++ src/loader/Loader.tsx
@@ -0,0 +1,64 @@
+import React, { PureComponent } from 'react'
+import cc from 'classcat'
+
+import prefix from '_utils'
+import { color, transition } from '_utils/branding'
+import CircleIcon from 'icon/circleIcon'
+import CheckIcon from 'icon/checkIcon'
+
+interface LoaderProps {
+  className?: Classcat.Class
+  inline?: boolean
+  size?: number
+  done?: boolean
+  onDoneAnimationEnd?: () => void
+}
+
+class Loader extends PureComponent<LoaderProps> {
+  static defaultProps: Partial<LoaderProps> = {
+    className: '',
+    inline: false,
+    size: 48,
+    done: false,
+    onDoneAnimationEnd() {},
+  }
+
+  validate = () => {
+    const timeout = parseInt(transition.duration.fast, 10) + transition.callbackDelay
+    setTimeout(this.props.onDoneAnimationEnd, timeout)
+  }
+
+  componentDidMount() {
+    if (this.props.done) {
+      this.validate()
+    }
+  }
+
+  componentWillReceiveProps({ done }: LoaderProps) {
+    if (done && done !== this.props.done) {
+      this.validate()
+    }
+  }
+
+  render() {
+    const { className, inline, size, done } = this.props
+    const sizes = {
+      width: `${size}px`,
+      height: `${size}px`,
+    }
+    return (
+      <div
+        className={cc([
+          prefix({ loader: true, 'loader--fullScreen': !inline, 'loader--done': done }),
+          className,
+        ])}
+        style={inline ? sizes : null}
+      >
+        {!done && <CircleIcon iconColor={color.success} size={size} spinning />}
+        {done && <CheckIcon iconColor={color.white} size={size / 2} validate />}
+      </div>
+    )
+  }
+}
+
+export default Loader
--- src/loader/story.tsx
+++ src/loader/story.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean, number } from '@storybook/addon-knobs'
 
-import Loader from 'loader'
+import Loader from './index'
 
 const stories = storiesOf('Loader', module)
 stories.addDecorator(withKnobs)
