65d637da427b245b85f2f721e48180e2d10f2ad1
Vincent Renaudin <vincent.renaudin@blablacar.com>
2019-07-30 16:48:17 +0200

    CSS-in-JS: Hamburger button

---
+++ src/hamburgerButton/HamburgerButton.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import cc from "classcat"
+
+export interface HamburgerButtonProps {
+  onClick: (event: React.MouseEvent<HTMLElement>) => void
+  open?: boolean
+  readonly className?: Classcat.Class
+}
+
+const HamburgerButton = ({ open = false, onClick, className }: HamburgerButtonProps) => (
+  <button className={cc([className])} aria-expanded={open} onClick={onClick}>
+    <i />
+  </button>
+)
+
+export default HamburgerButton
