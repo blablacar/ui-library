dc5f634fbbe3fc5d3c65870b795de55ea67890bf
Vincent Renaudin <vincent.renaudin@blablacar.com>
2019-07-30 11:39:58 +0200

    Rating - CSS in JS

--- src/rating/index.tsx
+++ src/rating/index.tsx
@@ -1,24 +1,18 @@
-import React from 'react'
-import cc from 'classcat'
+import styled from 'styled-components'
+import { color, space } from '_utils/branding'
 
-import style from 'rating/style'
-import Stars from 'stars'
+import Rating from './Rating'
 
-interface Rating {
-  className?: Classcat.Class
-  score: number
-  ratings: number
-  children: string
-}
+const StyledRating = styled(Rating)`
+  & {
+    display: flex;
+    align-items: center;
+  }
 
-const Rating = ({ className = '', score = 0, ratings = 0, children = '' }: Rating): JSX.Element => (
-  <div className={cc(['kirk-rating', className])}>
-    <Stars stars={score} />
-    <span>
-      {ratings} {children}
-    </span>
-    <style>{style}</style>
-  </div>
-)
+  & span {
+    color: ${color.secondaryText};
+    margin-left: ${space.s};
+  }
+`
 
-export default Rating
+export default StyledRating
