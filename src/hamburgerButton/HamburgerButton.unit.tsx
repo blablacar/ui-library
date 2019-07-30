9a74e957521d5e871f0d84c0968ab2e6536dc2ee
Vincent Renaudin <vincent.renaudin@blablacar.com>
2019-07-30 16:48:17 +0200

    CSS-in-JS: Hamburger button

--- src/hamburgerButton/HamburgerButton.unit.tsx
+++ src/hamburgerButton/HamburgerButton.unit.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import HamburgerButton from 'hamburgerButton'
+import HamburgerButton from './HamburgerButton'
 
 const defaultProps = {
   onClick() {},
