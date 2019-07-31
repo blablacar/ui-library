bfe2cb3cea1eb7c9a424fc3a8a40f298cc379adc
Pierre T <pierre.thirouin@blablacar.com>
2019-07-31 09:55:14 +0200

    Migrate Why to styled-component (#340)

---
+++ src/why/Why.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import cc from 'classcat'
+
+import QuestionIcon from 'icon/questionIcon'
+
+interface WhyProps {
+  readonly children: string
+  readonly title: string
+  readonly className?: Classcat.Class
+  readonly onClick?: () => void
+}
+
+const Why = ({ className, children, title, onClick }: WhyProps) => (
+  <button type="button" className={cc(['kirk-why', className])} title={title} onClick={onClick}>
+    <QuestionIcon />
+    <span>{children}</span>
+  </button>
+)
+
+export default Why
