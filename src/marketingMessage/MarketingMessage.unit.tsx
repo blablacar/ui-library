12f2c0d4ab0824ed39d33e7c2b436f9b6b3a5a44
Vincent Renaudin <vincent.renaudin@blablacar.com>
2019-10-22 13:04:39 +0200

    Create conversation marketing message for braze messages

---
+++ src/marketingMessage/MarketingMessage.unit.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import MarketingMessage from './MarketingMessage'
+
+it('should render a basic marketing conversation message', () => {
+  const message = shallow(
+    <MarketingMessage>
+      <span>message marketing</span>
+    </MarketingMessage>,
+  )
+  expect(message.text()).toContain('message marketing')
+})
