bed1a9fda62380a6198e03641d637ef9175662e8
Vincent Renaudin <vincent.renaudin@blablacar.com>
2019-11-12 07:41:27 +0100

    Fix margin on marketing  messages

--- src/marketingMessage/index.tsx
+++ src/marketingMessage/index.tsx
@@ -7,7 +7,7 @@ const StyledMarketingMessage = styled(MarketingMessage)`
   & {
     margin: 0;
     border: 1px solid ${color.divider};
-    margin: 0 ${space.xxl};
+    margin: 0 0 0 ${space.xxl};
     padding: ${space.m} ${space.l}
     border-radius: ${radius.l};
     border-top-left-radius: 0;
