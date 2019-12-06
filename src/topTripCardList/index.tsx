41b37a43e45ebdd2f720f612004da98494840d15
Cyrille Jouineau <cyrille.jouineau@blablacar.com>
2019-12-06 18:44:44 +0100

    Added TopTripCardList component

---
+++ src/topTripCardList/index.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+import { color, space, responsiveBreakpoints, componentSizes } from '_utils/branding'
+
+import TopTripCardList from './TopTripCardList'
+
+const wrapperHorizontalPadding = space.xl
+
+const StyledTopTripCardList = styled(TopTripCardList)`
+  & {
+    background-color: ${color.lightBackground};
+  }
+
+  & .kirk-topTripCardList {
+    margin: auto;
+    display: flex;
+    padding: ${space.l} ${wrapperHorizontalPadding};
+    box-sizing: border-box;
+    overflow: auto; /* Make TripCards scrollable horizontally */
+    -ms-overflow-style: none; /* Remove scrollbar visually IE 10+ */
+    scrollbar-width: none; /* Remove scrollbar visually Firefox */
+  }
+  /* Remove scrollbar webkit */
+  & .kirk-topTripCardList::-webkit-scrollbar {
+    display: none;
+  }
+
+  & .kirk-topTripCardList-wrapped {
+    max-width: ${componentSizes.wrapper};
+  }
+
+  & .kirk-tripCard {
+    flex-grow: 1;
+    margin-right: ${space.m};
+    background-color: ${color.white};
+  }
+
+  & .kirk-tripCard:last-child {
+    margin-right: 0;
+  }
+
+  /* The browser does not add right padding at the end of the scroll, CSS solution:
+  * https://blog.alexandergottlieb.com/overflow-scroll-and-the-right-padding-problem-a-css-only-solution-6d442915b3f4
+  */
+  & .kirk-tripCard:last-child::after {
+    content: '';
+    display: block;
+    position: absolute;
+    right: -${wrapperHorizontalPadding};
+    width: ${wrapperHorizontalPadding};
+    height: 1px;
+  }
+
+  @media (max-width: ${responsiveBreakpoints.small}) {
+    & .kirk-tripCard {
+      min-width: calc(100% - ${space.m});
+    }
+  }
+`
+
+export default StyledTopTripCardList
---
+++ src/topTripCardList/story.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { withKnobs, boolean } from '@storybook/addon-knobs'
+
+import TopTripCardList from 'topTripCardList'
+import TripCard from 'tripCard'
+
+const stories = storiesOf('TopTripCardList', module)
+stories.addDecorator(withKnobs)
+
+const tripCardConfig = {
+  ariaLabel: 'Pick-up point: Paris, Drop-off point: Bordeaux, Departure time: 09:00, warning',
+  href: '/',
+  itinerary: [
+    {
+      mainLabel: 'Paris',
+      subLabel: 'Porte de Vincennes',
+      time: '09:00',
+      isoDate: '2017-12-11T09:00',
+      distanceFromPoint: '1,5km',
+    },
+    {
+      mainLabel: 'Bordeaux',
+      subLabel: 'Gare Bordeaux Saint-Jean',
+      time: '12:00',
+      isoDate: '2017-12-11T12:00',
+      distanceFromPoint: '8km',
+    },
+  ],
+  price: '8,00€',
+  flags: {
+    ladiesOnly: true,
+    maxTwo: true,
+    autoApproval: true,
+  },
+  metaUrl: 'Meta URL',
+  badge: 'Cheapest',
+  title: '',
+}
+
+stories.add('TopTripCardList - default', () => (
+  <TopTripCardList isWrapped={boolean('isWrapped', false)}>
+    <TripCard {...tripCardConfig} />
+    <TripCard
+      {...tripCardConfig}
+      driver={{
+        avatarUrl: '//placehold.it/500x500',
+        firstName: 'Jane',
+      }}
+      badge="Closest"
+    />
+  </TopTripCardList>
+))
+
+stories.add('TopTripCardList - 4 trip cards', () => (
+  <TopTripCardList isWrapped={boolean('isWrapped', false)}>
+    <TripCard {...tripCardConfig} />
+    <TripCard
+      {...tripCardConfig}
+      driver={{
+        avatarUrl: '//placehold.it/500x500',
+        firstName: 'Jane',
+      }}
+      badge="Closest"
+    />
+    <TripCard {...tripCardConfig} />
+    <TripCard
+      {...tripCardConfig}
+      driver={{
+        avatarUrl: '//placehold.it/500x500',
+        firstName: 'Jane',
+      }}
+      badge="Closest"
+    />
+  </TopTripCardList>
+))
