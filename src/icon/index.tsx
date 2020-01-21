b6573d1ad87ee0dca102620ecdddc208ce670a66
Jérôme Smadja <jerome.smadja@blablacar.com>
2020-01-21 12:05:42 +0100

    Added plugUsbSocketIcon, priorityServiceIcon, quietIcon, qrCode, recliningSeat, seatBelt, seatWithTable, snack, airConditioning, audio, bikeArea, blanket, child, comfortSeat, crew, drink, genericAmenity, handLuggage, holdLuggage, legRest, legRoom, light, magazine, meal, seatSliding, sleep, standardSeat, videoSystem, vipLounge, wc, wifi icons

--- src/icon/index.tsx
+++ src/icon/index.tsx
@@ -1,31 +1,44 @@
+import AirConditioningIcon from './airConditioning'
 import ArrowIcon from './arrowIcon'
+import AudioIcon from './audio'
 import AvatarIcon from './avatarIcon'
 import BanIcon from './banIcon'
 import BankIcon from './bankIcon'
 import BellIcon from './bellIcon'
+import BikeAreaIcon from './bikeArea'
+import BlanketIcon from './blanket'
 import BubbleIcon from './bubbleIcon'
+import BusIcon from './busIcon'
 import CallIcon from './callIcon'
 import CarIcon from './carIcon'
+import CarpoolIcon from './carpoolIcon'
 import CheckIcon from './checkIcon'
 import CheckShieldIcon from './checkShieldIcon'
 import ChevronIcon from './chevronIcon'
+import ChildIcon from './child'
 import CircleIcon from './circleIcon'
 import ClockIcon from './clockIcon'
 import CoinIcon from './coinIcon'
 import ComfortIcon from './comfortIcon'
+import ComfortSeatIcon from './comfortSeat'
 import CreditCardIcon from './creditCardIcon'
+import CrewIcon from './crew'
 import CrossDiscIcon from './crossDiscIcon'
-import CrossIcon from './crossIcon'
 import CrosshairIcon from './crosshairIcon'
+import CrossIcon from './crossIcon'
 import DepartureIcon from './departureIcon'
 import DetourIcon from './detourIcon'
 import DirectionIcon from './directionIcon'
 import DoubleArrowIcon from './doubleArrowIcon'
-import EyeIcon from './eyeIcon'
+import DrinkIcon from './drink'
 import ExclamationIcon from './exclamationIcon'
+import EyeIcon from './eyeIcon'
 import FacebookIcon from './facebookIcon'
 import FlagsIcon from './flagsIcon'
+import GenericAmenityIcon from './genericAmenity'
 import GiftIcon from './giftIcon'
+import HandLuggageIcon from './handLuggage'
+import HoldLuggageIcon from './holdLuggage'
 import HomeIcon from './homeIcon'
 import HourglassIcon from './hourglassIcon'
 import IdCardVerifiedIcon from './idCardVerifiedIcon'
@@ -33,69 +46,100 @@ import IdCheckIcon from './idCheckIcon'
 import InfoIcon from './infoIcon'
 import InstagramIcon from './instagramIcon'
 import LadyIcon from './ladyIcon'
+import LegRestIcon from './legRest'
+import LegRoomIcon from './legRoom'
+import LightIcon from './light'
 import LightningIcon from './lightningIcon'
+import MagazineIcon from './magazine'
 import MailIcon from './mailIcon'
 import MapIcon from './mapIcon'
+import MealIcon from './meal'
 import MeetingPointIcon from './meetingPoint'
 import MinusIcon from './minusIcon'
 import MusicIcon from './musicIcon'
 import MyRidesIcon from './myRides'
 import NewspaperIcon from './newspaperIcon'
 import OdnoklassnikiIcon from './odnoklassnikiIcon'
-import PetIcon from './petIcon'
+import PetIcon from './pet'
 import PhotoIcon from './photoIcon'
 import PinIcon from './pinIcon'
 import PinterestIcon from './pinterestIcon'
+import PlugUsbSocketIcon from './plugUsbSocket'
 import PlusIcon from './plusIcon'
+import PriorityServiceIcon from './priorityService'
 import ProfileIcon from './profileIcon'
 import ProximityIcon from './proximityIcon'
+import QrCodeIcon from './qrCode'
 import QuestionIcon from './questionIcon'
+import QuietIcon from './quiet'
+import RecliningSeatIcon from './recliningSeat'
 import SearchIcon from './searchIcon'
+import SeatBeltIcon from './seatBelt'
+import SeatSlidingIcon from './seatSliding'
+import SeatWithTableIcon from './seatWithTable'
 import SendIcon from './sendIcon'
 import SendMessageIcon from './sendMessageIcon'
 import ShareIcon from './shareIcon'
+import SleepIcon from './sleep'
 import SmokeIcon from './smokeIcon'
+import SnackIcon from './snack'
 import SpeakIcon from './speakIcon'
+import StandartSeatIcon from './standardSeat'
 import StarIcon from './starIcon'
-import BusIcon from './busIcon'
-import CarpoolIcon from './carpoolIcon'
 import TicketIcon from './ticketIcon'
 import TrashIcon from './trashIcon'
 import TwitterIcon from './twitterIcon'
+import VideoSystemIcon from './videoSystem'
+import VipLoungeIcon from './vipLounge'
 import VkontakteIcon from './vkontakteIcon'
 import WarningIcon from './warningIcon'
+import WcIcon from './wc'
 import WheelchairIcon from './wheelchair'
+import WifiIcon from './wifi'
 import YoutubeIcon from './youtubeIcon'
 
 export {
+  AirConditioningIcon,
   ArrowIcon,
+  AudioIcon,
   AvatarIcon,
   BanIcon,
   BankIcon,
   BellIcon,
+  BikeAreaIcon,
+  BlanketIcon,
   BubbleIcon,
-  CarIcon,
+  BusIcon,
   CallIcon,
+  CarIcon,
+  CarpoolIcon,
   CheckIcon,
   CheckShieldIcon,
   ChevronIcon,
+  ChildIcon,
   CircleIcon,
   ClockIcon,
   CoinIcon,
   ComfortIcon,
+  ComfortSeatIcon,
   CreditCardIcon,
+  CrewIcon,
   CrossDiscIcon,
-  CrossIcon,
   CrosshairIcon,
+  CrossIcon,
   DepartureIcon,
   DetourIcon,
   DirectionIcon,
   DoubleArrowIcon,
-  EyeIcon,
+  DrinkIcon,
   ExclamationIcon,
+  EyeIcon,
   FacebookIcon,
   FlagsIcon,
+  GenericAmenityIcon,
   GiftIcon,
+  HandLuggageIcon,
+  HoldLuggageIcon,
   HomeIcon,
   HourglassIcon,
   IdCardVerifiedIcon,
@@ -103,9 +147,14 @@ export {
   InfoIcon,
   InstagramIcon,
   LadyIcon,
+  LegRestIcon,
+  LegRoomIcon,
+  LightIcon,
   LightningIcon,
+  MagazineIcon,
   MailIcon,
   MapIcon,
+  MealIcon,
   MeetingPointIcon,
   MinusIcon,
   MusicIcon,
@@ -116,24 +165,37 @@ export {
   PhotoIcon,
   PinIcon,
   PinterestIcon,
+  PlugUsbSocketIcon,
   PlusIcon,
+  PriorityServiceIcon,
   ProfileIcon,
   ProximityIcon,
+  QrCodeIcon,
   QuestionIcon,
+  QuietIcon,
+  RecliningSeatIcon,
   SearchIcon,
+  SeatBeltIcon,
+  SeatSlidingIcon,
+  SeatWithTableIcon,
   SendIcon,
   SendMessageIcon,
   ShareIcon,
+  SleepIcon,
   SmokeIcon,
+  SnackIcon,
   SpeakIcon,
+  StandartSeatIcon,
   StarIcon,
-  BusIcon,
-  CarpoolIcon,
   TicketIcon,
   TrashIcon,
   TwitterIcon,
+  VideoSystemIcon,
+  VipLoungeIcon,
   VkontakteIcon,
   WarningIcon,
+  WcIcon,
   WheelchairIcon,
+  WifiIcon,
   YoutubeIcon,
 }
