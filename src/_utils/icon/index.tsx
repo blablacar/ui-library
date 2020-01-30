5aaf613314fe146fab8ed3c7c86afa154f90cef8
Michael Cellier <mcellier@users.noreply.github.com>
2020-01-30 09:07:50 +0100

    Update MeetingPointIcon (new active state, optional shadow) (#515)

--- src/_utils/icon/index.tsx
+++ src/_utils/icon/index.tsx
@@ -1,11 +1,17 @@
 import styled from 'styled-components'
 
+import { shadow } from '_utils/branding'
 import BaseIcon from './BaseIcon'
 
 const StyledBaseIcon = styled(BaseIcon)`
   &.kirk-icon-wrapper {
     display: inline-block;
     position: relative;
+  }
+
+  &.kirk-icon-shadowed {
+    border-radius: 50%;
+    box-shadow: ${shadow.icon};
   }
 
   & .kirk-icon-badge {
