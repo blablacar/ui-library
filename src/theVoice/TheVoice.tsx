27e5b8ae35f9fe8de7d681c5f7dbfb63707ed6c8
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-02-17 16:13:28 +0100

    [BBC-5337] Upgrade  components to receive textWithBackground color

--- src/theVoice/TheVoice.tsx
+++ src/theVoice/TheVoice.tsx
@@ -5,12 +5,16 @@ export interface TheVoiceProps {
   readonly id?: string
   readonly className?: Classcat.Class
   readonly children: string
+  readonly isInverted?: boolean
 }
 
-const TheVoice = ({ id, className, children }: TheVoiceProps) => (
-  <Title id={id} className={className} headingLevel="1">
-    {children}
-  </Title>
-)
+const TheVoice = ({ id, className, children, isInverted = false }: TheVoiceProps) => {
+  const classes = isInverted ? `kirk-title--inverse ${className}` : className
+  return (
+    <Title id={id} className={classes} headingLevel="1">
+      {children}
+    </Title>
+  )
+}
 
 export default TheVoice
