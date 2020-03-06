2e1105a0d1f66d1299205e9da7c6d32eae4510de
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-03-06 10:48:36 +0100

    [BBC-1272] Revert: Add general documentation

--- src/typography/body/story.tsx
+++ src/typography/body/story.tsx
@@ -7,26 +7,18 @@ import { color } from '_utils/branding'
 import Section from 'layout/section/baseSection'
 import TextBody from './index'
 
-import readme from './readme.md'
-
 const stories = storiesOf('Brand|typography', module)
 stories.addDecorator(withKnobs)
 
-stories.add(
-  'TextBody',
-  () => (
-    <Section>
-      <h1>
-        <TextBody isInverted={boolean('isInverted', false)}>
-          {text('Text1', 'The quick\n brown fox jumps\n over the lazy\n dog')}
-        </TextBody>
-      </h1>
-      <h1 style={{ backgroundColor: color.primary }}>
-        <TextBody isInverted>
-          {text('Text2', 'The quick brown fox jumps over the lazy dog')}
-        </TextBody>
-      </h1>
-    </Section>
-  ),
-  { readme: { content: readme } },
-)
+stories.add('TextBody', () => (
+  <Section>
+    <h1>
+      <TextBody isInverted={boolean('isInverted', false)}>
+        {text('Text1', 'The quick\n brown fox jumps\n over the lazy\n dog')}
+      </TextBody>
+    </h1>
+    <h1 style={{ backgroundColor: color.primary }}>
+      <TextBody isInverted>{text('Text2', 'The quick brown fox jumps over the lazy dog')}</TextBody>
+    </h1>
+  </Section>
+))
--- src/typography/story.tsx
+++ src/typography/story.tsx
@@ -15,86 +15,94 @@ import TextSubHeaderStrong from './subHeaderStrong'
 import TextDisplay2 from './display2'
 import TextDisplay1 from './display1'
 
+import readme from './specifications/typography.md'
+
 const stories = storiesOf('Brand|typography', module)
 
-stories.add('ALL', () => (
-  <Fragment>
-    <Section>
-      <h1 style={{ marginBottom: space.xxl }}>
-        <p style={{ marginBottom: space.s }}>
-          <TextBody>Display2</TextBody>
-        </p>
-        <TextDisplay2>
-          Medium 82px <br /> Height 82px
-        </TextDisplay2>
-      </h1>
-      <h2 style={{ marginBottom: space.xxl }}>
-        <p style={{ marginBottom: space.s }}>
-          <TextBody>Display1</TextBody>
-        </p>
-        <TextDisplay1>
-          Medium 30px <br /> Height 32px
-        </TextDisplay1>
-      </h2>
-      <h3 style={{ marginBottom: space.xxl }}>
-        <p style={{ marginBottom: space.s }}>
-          <TextBody>SubHeader</TextBody>
-        </p>
-        <TextSubHeader>
-          Regular 22px <br /> Height 24px
-        </TextSubHeader>
-      </h3>
-      <h3 style={{ marginBottom: space.xxl }}>
-        <p style={{ marginBottom: space.s }}>
-          <TextBody>SubHeaderStrong</TextBody>
-        </p>
-        <TextSubHeaderStrong>
-          Medium 22px <br /> Height 24px
-        </TextSubHeaderStrong>
-      </h3>
+stories.add(
+  'ALL',
+  () => (
+    <Fragment>
+      <Section>
+        <h1 style={{ marginBottom: space.xxl }}>
+          <p style={{ marginBottom: space.s }}>
+            <TextBody>Display2</TextBody>
+          </p>
+          <TextDisplay2>
+            Medium 82px <br /> Height 82px
+          </TextDisplay2>
+        </h1>
+        <h2 style={{ marginBottom: space.xxl }}>
+          <p style={{ marginBottom: space.s }}>
+            <TextBody>Display1</TextBody>
+          </p>
+          <TextDisplay1>
+            Medium 30px <br /> Height 32px
+          </TextDisplay1>
+        </h2>
+        <h3 style={{ marginBottom: space.xxl }}>
+          <p style={{ marginBottom: space.s }}>
+            <TextBody>SubHeader</TextBody>
+          </p>
+          <TextSubHeader>
+            Regular 22px <br /> Height 24px
+          </TextSubHeader>
+        </h3>
+        <h3 style={{ marginBottom: space.xxl }}>
+          <p style={{ marginBottom: space.s }}>
+            <TextBody>SubHeaderStrong</TextBody>
+          </p>
+          <TextSubHeaderStrong>
+            Medium 22px <br /> Height 24px
+          </TextSubHeaderStrong>
+        </h3>
 
-      <p style={{ marginBottom: space.s }}>
-        <TextBody>Title</TextBody>
-      </p>
-      <h4 style={{ marginTop: 0, marginBottom: space.xxl }}>
-        <TextTitle>
-          Regular 18px <br /> Height 20px
-        </TextTitle>
-      </h4>
-
-      <p style={{ marginBottom: space.s }}>
-        <TextBody>TitleStrong</TextBody>
-      </p>
-      <h4 style={{ marginTop: 0, marginBottom: space.xxl }}>
-        <TextTitleStrong>
-          Medium 18px <br /> Height 20px
-        </TextTitleStrong>
-      </h4>
-
-      <p style={{ marginBottom: space.xxl }}>
-        <TextBody>Body</TextBody>
-        <br />
-        <TextBody>
-          Regular 16px <br /> Height 20px
-        </TextBody>
-      </p>
-
-      <p style={{ marginBottom: space.xxl }}>
-        <TextBody>bodyStrong</TextBody>
-        <br />
-        <TextBodyStrong>
-          Medium 16px <br /> Height 20px
-        </TextBodyStrong>
-      </p>
-
-      <figcaption>
         <p style={{ marginBottom: space.s }}>
-          <TextBody>Caption</TextBody>
+          <TextBody>Title</TextBody>
         </p>
-        <TextCaption>
-          Regular 13px <br /> Height 16px
-        </TextCaption>
-      </figcaption>
-    </Section>
-  </Fragment>
-))
+        <h4 style={{ marginTop: 0, marginBottom: space.xxl }}>
+          <TextTitle>
+            Regular 18px <br /> Height 20px
+          </TextTitle>
+        </h4>
+
+        <p style={{ marginBottom: space.s }}>
+          <TextBody>TitleStrong</TextBody>
+        </p>
+        <h4 style={{ marginTop: 0, marginBottom: space.xxl }}>
+          <TextTitleStrong>
+            Medium 18px <br /> Height 20px
+          </TextTitleStrong>
+        </h4>
+
+        <p style={{ marginBottom: space.xxl }}>
+          <TextBody>Body</TextBody>
+          <br />
+          <TextBody>
+            Regular 16px <br /> Height 20px
+          </TextBody>
+        </p>
+
+        <p style={{ marginBottom: space.xxl }}>
+          <TextBody>bodyStrong</TextBody>
+          <br />
+          <TextBodyStrong>
+            Medium 16px <br /> Height 20px
+          </TextBodyStrong>
+        </p>
+
+        <figcaption>
+          <p style={{ marginBottom: space.s }}>
+            <TextBody>Caption</TextBody>
+          </p>
+          <TextCaption>
+            Regular 13px <br /> Height 16px
+          </TextCaption>
+        </figcaption>
+      </Section>
+    </Fragment>
+  ),
+  {
+    readme: { content: readme },
+  },
+)
