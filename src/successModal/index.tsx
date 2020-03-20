cbab49937caf4a4d6c0bffbf2ddb76ea4caa498c
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-03-20 17:08:23 +0100

    [BBC-6972] Converting property SIZE into Breakpoint

--- src/successModal/index.tsx
+++ src/successModal/index.tsx
@@ -1,71 +1,34 @@
 import styled from 'styled-components'
-import { color, font, space, modalSize } from '_utils/branding'
+import { color, space, modalSize, responsiveBreakpoints } from '_utils/branding'
 import SuccessModal from './SuccessModal'
 
 const footerHeight = '96px' /* = padding + content */
 
 const StyledSuccessModal = styled(SuccessModal)`
-  text-align: center;
-
   & .kirk-successModal {
+    text-align: center;
     background-color: ${color.successBackground};
     color: ${color.white};
   }
 
-  & .kirk-modal.kirk-successModal .kirk-modal-dialog {
-    margin: 0 auto;
-    max-width: ${modalSize.m};
-    height: 100%;
-    padding: ${space.xl} 0 ${footerHeight} 0;
-    background-color: ${color.successBackground};
-  }
+  & .kirk-modal.kirk-successModal {
+    .kirk-modal-dialog {
+      margin: 0 auto;
+      max-width: ${modalSize.m};
+      height: 100%;
+      padding: ${space.xl} 0 ${footerHeight} 0;
+      background-color: ${color.successBackground}; /* override */
+    }
 
-  /* Stronger selector to override Modal component styling */
-  &.kirk-modal-dimmer .kirk-modal.kirk-successModal .kirk-modal-dialog.kirk-modal-dialog {
-    width: auto;
-  }
-
-  & .kirk-modal--large.kirk-successModal .kirk-modal-dialog {
-    padding: ${space.xl} 0;
-  }
-
-  & .kirk-modal.kirk-successModal .kirk-modal-body {
-    padding: ${space.xl};
-    font-size: ${font.l.size};
-    line-height: ${font.l.lineHeight};
-    text-align: center;
-    align-items: center;
-  }
-
-  & .kirk-modal--large.kirk-successModal .kirk-modal-body {
-    position: relative;
-    display: flex;
-  }
-
-  & .kirk-modal--large .kirk-successModal-bodyItem {
-    width: 50%;
-  }
-
-  & .kirk-modal--large .kirk-successModal-bodyItem:nth-child(odd) {
-    padding-right: ${space.xl};
-  }
-
-  & .kirk-modal--large .kirk-successModal-bodyItem:nth-child(even) {
-    padding-left: ${space.xl};
+    .kirk-modal-body {
+      padding: ${space.xl};
+      align-items: center;
+    }
   }
 
   & .kirk-successModal-image {
     width: 80%;
     margin-bottom: ${space.xl};
-  }
-
-  & .kirk-modal--small .kirk-successModal-image {
-    max-height: 33vh;
-    object-fit: contain;
-  }
-
-  & .kirk-modal--large .kirk-successModal-image {
-    margin-bottom: 0;
   }
 
   & .kirk-successModal-footer {
@@ -78,19 +41,67 @@ const StyledSuccessModal = styled(SuccessModal)`
     text-align: center;
   }
 
-  & .kirk-modal--large .kirk-successModal-footer {
-    position: relative;
-    display: block;
-    margin: 0 auto;
-  }
-
-  & .kirk-modal--large .kirk-successModal-footer .kirk-button {
-    margin: ${space.l} ${space.xl} 0;
-  }
-
   & .kirk-successModal-confirmButton {
     vertical-align: bottom; /* vertical alignment to be fixed in Button */
     border: 0; /* content alignment to be fixed in Button */
+  }
+
+  /* Stronger selector to override Modal component styling */
+  &.kirk-modal-dimmer .kirk-modal.kirk-successModal .kirk-modal-dialog.kirk-modal-dialog {
+    width: auto;
+  }
+
+  /* Small */
+
+  & .kirk-modal--small .kirk-successModal-image {
+    max-height: 33vh;
+    object-fit: contain;
+  }
+
+  /* Large */
+
+  @media (${responsiveBreakpoints.isMediaLarge}) {
+    .kirk-successModal {
+      .kirk-modal-body {
+        position: relative;
+        display: flex;
+      }
+
+      .kirk-modal-dialog {
+        padding: ${space.xl} 0;
+      }
+    }
+
+    .kirk-successModal-bodyItem {
+      &:nth-child(odd) {
+        padding-right: ${space.xl};
+      }
+      &:nth-child(even) {
+        padding-left: ${space.xl};
+      }
+    }
+
+    .kirk-successModal-bodyItem {
+      width: 50%;
+    }
+
+    .kirk-successModal-image {
+      margin-bottom: 0;
+    }
+
+    &.kirk-successModal .kirk-modal-dialog {
+      padding: ${space.xl} 0;
+    }
+
+    .kirk-successModal-footer {
+      position: relative;
+      display: block;
+      margin: 0 auto;
+
+      .kirk-button {
+        margin: ${space.l} ${space.xl} 0;
+      }
+    }
   }
 `
 
