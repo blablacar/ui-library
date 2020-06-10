27eba23b97779c1480ee1c7dd8e39c1a686bf1c2
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-06-10 22:58:13 +0200

    Order stories a-z

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,10 +8,7 @@ addDecorator(withA11y)
 
 addParameters({
   options: {
-    name: 'PIXAR - BlaBlaCar UI',
-    options: {
-      storySort: (a, b) =>
-        a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-    },
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 })
