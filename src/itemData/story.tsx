ab80fbfaadf80c32375b55fa0e7f593b76eb006d
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-06-12 14:35:20 +0200

    [BBC-8438] `ItemBigData` Component (#750)
    * Add new component ItemBigData based on Item

--- src/itemData/story.tsx
+++ src/itemData/story.tsx
@@ -12,29 +12,80 @@ import { BaseSection as Section } from '../layout/section/baseSection'
 const stories = storiesOf('Widgets|ItemData', module)
 stories.addDecorator(withKnobs)
 
-stories.add('Default', () => {
+stories.add('Data', () => {
   const ismainTitleButtonAddon = boolean('Title button info', true)
   const dataStrikeThrough = boolean('Data strikethrough', false)
+  const mainTitleButtonAddon = ismainTitleButtonAddon ? (
+    <Button status={ButtonStatus.UNSTYLED} isBubble onClick={action('Info clicked')}>
+      <InfoIcon iconColor={color.blue} title="More info" />
+    </Button>
+  ) : null
 
   return (
     <Section>
       <ItemData
-        mainTitle={text('Main title')}
-        ariaLabel={text('Aria label', 'Complementary info')}
-        mainInfo={text('Main info', 'Main information')}
-        data={text('Data', 'Data')}
-        dataInfo={text('Data info')}
-        dataAriaLabel={text('Data aria label', 'Data aria label')}
+        mainTitle={text(
+          'mainTitle',
+          'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+        )}
+        ariaLabel={text('Aria label', 'Additional info')}
+        mainInfo={text('mainInfo', 'Main information')}
+        data={text('data', 'Data')}
+        dataInfo={text('dataInfo', 'Data info')}
+        dataAriaLabel={text('dataAriaLabel', 'Data aria label')}
         dataStrikeThrough={dataStrikeThrough}
         disabled={boolean('Disabled', false)}
-        mainTitleButtonAddon={
-          ismainTitleButtonAddon ? (
-            <Button status={ButtonStatus.UNSTYLED} isBubble onClick={action('Info clicked')}>
-              <InfoIcon iconColor={color.blue} title="More info" />
-            </Button>
-          ) : null
-        }
+        mainTitleButtonAddon={mainTitleButtonAddon}
         tag={<li />}
+      />
+    </Section>
+  )
+})
+
+stories.add('isDisabled', () => {
+  const ismainTitleButtonAddon = boolean('Title button info', true)
+  const dataStrikeThrough = boolean('Data strikethrough', true)
+  const mainTitleButtonAddon = ismainTitleButtonAddon ? (
+    <Button status={ButtonStatus.UNSTYLED} isBubble onClick={action('Info clicked')}>
+      <InfoIcon iconColor={color.blue} title="More info" />
+    </Button>
+  ) : null
+
+  return (
+    <Section>
+      <ItemData
+        mainTitle={text(
+          'mainTitle',
+          'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+        )}
+        ariaLabel={text('Aria label', 'Additional info')}
+        mainInfo={text('mainInfo', 'Main information')}
+        data={text('data', 'Data')}
+        dataInfo={text('dataInfo', 'Data info')}
+        dataAriaLabel={text('dataAriaLabel', 'Data aria label')}
+        dataStrikeThrough={dataStrikeThrough}
+        disabled={boolean('disabled', true)}
+        mainTitleButtonAddon={mainTitleButtonAddon}
+        tag={<li />}
+      />
+    </Section>
+  )
+})
+
+stories.add('withDataStrikeThrough', () => {
+  const dataStrikeThrough = boolean('DdataStrikeThrough', true)
+
+  return (
+    <Section>
+      <ItemData
+        mainTitle={text('mainTitle', 'Main title')}
+        ariaLabel={text('ariaLabel', 'Additional info')}
+        mainInfo={text('mainInfo', 'Main information')}
+        data={text('data', 'Data')}
+        dataInfo={text('dataInfo', 'Data info')}
+        dataAriaLabel={text('dataAriaLabel', 'Data aria label')}
+        dataStrikeThrough={dataStrikeThrough}
+        disabled={false}
       />
     </Section>
   )
