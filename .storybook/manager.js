db9be294e74b0e578de114b94f2d07fe35ff4d4a
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-06-16 22:52:57 +0200

    Theming Storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/addons'
-import pixar from './theme-pixar'
+import theme from './theme'
 
 addons.setConfig({
-  theme: pixar,
+  theme,
 })
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,28 @@
+import React from 'react'
 import { addDecorator, addParameters } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
 import { withA11y } from '@storybook/addon-a11y'
-// import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
-addDecorator(withKnobs)
-addDecorator(withA11y)
+import { BaseSection as Section } from '../src/layout/section/baseSection'
+
+import '../stories/stories.css'
 
 addParameters({
   options: {
+    showPanel: true,
+    isToolshown: true,
     storySort: (a, b) =>
       a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 })
+
+addDecorator(withKnobs)
+addDecorator(story => (
+  <div
+    style={{
+      margin: '3rem auto',
+    }}
+  >
+    <Section>{story()}</Section>
+  </div>
+))
