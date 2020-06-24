80b25310d10d9c000e2915ac9e300e4ff539ed75
Simon Relet <simon.relet@blablacar.com>
2020-06-24 16:06:40 +0200

    Use RTL in Tabs tests

--- src/tabs/Tabs.unit.tsx
+++ src/tabs/Tabs.unit.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
-import { mount } from 'enzyme'
 
-import { MailIcon } from '../icon/mailIcon'
-import { Tabs } from './Tabs'
+import {
+  fireEvent,
+  getAllByRole,
+  getByRole,
+  queryByRole,
+  render,
+  screen,
+} from '@testing-library/react'
 
-const defaultTabsConfig = {
+import { Tab, Tabs, TabsProps } from './Tabs'
+
+const defaultProps: TabsProps = {
   activeTabId: 'tab1',
   tabs: [
     {
@@ -26,245 +32,217 @@ const defaultTabsConfig = {
   ],
 }
 
-const iconTabsConfig = {
-  activeTabId: 'iconTab1',
-  tabs: [
-    {
-      ...defaultTabsConfig.tabs[0],
-      id: 'iconTab1',
-      icon: <MailIcon />,
-    },
-    {
-      ...defaultTabsConfig.tabs[1],
-      id: 'iconTab2',
-    },
-    {
-      ...defaultTabsConfig.tabs[2],
-      id: 'iconTab3',
-      icon: <MailIcon />,
-      showIconOnly: true,
-    },
-  ],
+function createProps(props: Partial<TabsProps> = {}): TabsProps {
+  return { ...defaultProps, ...props }
 }
 
 describe('Rendering testing', () => {
-  it('should render properly', () => {
-    const tabsAsJson = renderer
-      .create(<Tabs tabs={defaultTabsConfig.tabs} activeTabId={defaultTabsConfig.activeTabId} />)
-      .toJSON()
-    expect(tabsAsJson).toMatchSnapshot()
+  it('should render the tabs', () => {
+    const props = createProps()
+    render(<Tabs {...props} />)
+    expect(screen.getAllByRole('tab')).toHaveLength(3)
+    props.tabs.forEach((tab: Tab) => {
+      expect(screen.getByRole('tab', { name: tab.label })).toHaveTextContent(tab.label)
+    })
   })
 
-  it('should render properly with icons', () => {
-    const tabsAsJson = renderer
-      .create(<Tabs tabs={iconTabsConfig.tabs} activeTabId={iconTabsConfig.activeTabId} />)
-      .toJSON()
-    expect(tabsAsJson).toMatchSnapshot()
-  })
-
-  it('Should update the panel content', () => {
-    const onChange = jest.fn()
-    const wrapper = mount(
-      <Tabs
-        onChange={onChange}
-        tabs={defaultTabsConfig.tabs}
-        activeTabId={defaultTabsConfig.activeTabId}
-      />,
+  it('should render the active tab content', () => {
+    const props = createProps()
+    render(<Tabs {...props} />)
+    expect(screen.getAllByRole('tabpanel')).toHaveLength(1)
+    expect(screen.getByRole('tabpanel', { name: props.tabs[0].label })).toHaveTextContent(
+      'Content for first tab',
     )
+  })
 
-    let tabpanel = wrapper.find('[role="tabpanel"]').at(0)
-    expect(tabpanel.text()).toBe('Content for first tab')
+  it('should render the icon and label', () => {
+    const props: TabsProps = {
+      activeTabId: '1',
+      tabs: [
+        {
+          id: '1',
+          label: 'With icon',
+          panelContent: <div />,
+          icon: <span role="img" aria-label="image" />,
+        },
+      ],
+    }
 
-    // Simulate a click on tab 2.
-    wrapper
-      .find('[role="tab"]')
-      .at(1)
-      .simulate('click')
+    render(<Tabs {...props} />)
+    const tab = screen.getByRole('tab', { name: 'With icon' })
+    expect(tab).toHaveTextContent('With icon')
+    expect(getByRole(tab, 'img', { name: 'image' })).toBeInTheDocument()
+  })
 
-    expect(onChange).toHaveBeenCalledWith('tab2')
+  it('should only render the icon when showIconOnly is true', () => {
+    const props: TabsProps = {
+      activeTabId: '1',
+      tabs: [
+        {
+          id: '1',
+          label: 'With icon',
+          panelContent: <div />,
+          icon: <span role="img" aria-label="image" />,
+          showIconOnly: true,
+        },
+      ],
+    }
 
-    tabpanel = wrapper.find('[role="tabpanel"]').at(1)
-    expect(tabpanel.text()).toBe('Content for second tab')
+    render(<Tabs {...props} />)
+    const tab = screen.getByRole('tab', { name: 'With icon' })
+    expect(tab).toHaveTextContent('')
+    expect(getByRole(tab, 'img', { name: 'image' })).toBeInTheDocument()
+  })
 
-    // Simulate a click on tab 1.
-    wrapper
-      .find('[role="tab"]')
-      .at(0)
-      .simulate('click')
+  it('Should call onChange when a tab is selected', () => {
+    const props = createProps({ onChange: jest.fn() })
+    render(<Tabs {...props} />)
+    fireEvent.click(screen.getByRole('tab', { name: 'Tab 2' }))
+    expect(props.onChange).toHaveBeenCalledWith('tab2')
+  })
 
-    expect(onChange).toHaveBeenCalledWith('tab1')
-
-    tabpanel = wrapper.find('[role="tabpanel"]').at(0)
-    expect(tabpanel.text()).toBe('Content for first tab')
+  it('Should update the tabpanel when a tab is selected', () => {
+    const props = createProps()
+    render(<Tabs {...props} />)
+    fireEvent.click(screen.getByRole('tab', { name: props.tabs[1].label }))
+    expect(screen.getByRole('tabpanel', { name: props.tabs[1].label })).toHaveTextContent(
+      'Content for second tab',
+    )
   })
 })
 
 describe('ARIA roles', () => {
   it('Should generate proper ARIA roles', () => {
-    const wrapper = mount(
-      <Tabs tabs={defaultTabsConfig.tabs} activeTabId={defaultTabsConfig.activeTabId} />,
-    )
+    const props = createProps()
+    render(<Tabs {...props} />)
 
     // Verify we have a tablist role element.
-    const tablist = wrapper.find('[role="tablist"]')
-    expect(tablist.exists()).toBe(true)
-    expect(tablist.length).toBe(1)
+    const tablist = screen.getByRole('tablist')
+    expect(tablist).toBeInTheDocument()
 
     // Verify we have correct tabs inside the tablist role element.
-    const tabs = tablist.find('[role="tab"]')
-    expect(tabs.exists()).toBe(true)
-    expect(tabs.length).toBe(3)
-    expect(tabs.get(0).props['aria-controls']).toBe('tab1_panel')
-    expect(tabs.get(1).props['aria-controls']).toBe('tab2_panel')
-    expect(tabs.get(2).props['aria-controls']).toBe('tab3_panel')
+    const tabs = getAllByRole(tablist, 'tab')
+    expect(tabs).toHaveLength(3)
+    expect(tabs[0]).toHaveAttribute('aria-controls', 'tab1_panel')
+    expect(tabs[1]).toHaveAttribute('aria-controls', 'tab2_panel')
+    expect(tabs[2]).toHaveAttribute('aria-controls', 'tab3_panel')
 
     // Verify we have one tabpanel.
-    const tabpanel = wrapper.find('[role="tabpanel"]')
-    expect(tabpanel.exists()).toBe(true)
-    expect(tabpanel.length).toBe(3)
+    const tabpanel = screen.getByRole('tabpanel', { name: 'Tab 1' })
     // Verify the tabpanel generated id (that should match the aria-controls
     // of the selected tab.
-    const expectedTabPanelId = `${defaultTabsConfig.activeTabId}_panel`
-    expect(tabpanel.get(0).props.id).toBe(expectedTabPanelId)
-    expect(tabpanel.get(0).props['aria-labelledby']).toBe('tab1')
+    expect(tabpanel).toHaveAttribute('id', 'tab1_panel')
 
     // Verify the tabpanel is not in the tablist or in a tab.
-    const panelInTabList = tablist.find('[role="tabpanel"]')
-    expect(panelInTabList.exists()).toBe(false)
-    const panelInTabs = tabs.find('[role="tabpanel"]')
-    expect(panelInTabs.exists()).toBe(false)
+    expect(queryByRole(tablist, 'tabpanel')).not.toBeInTheDocument()
 
-    // Verify the selected tab ARIA state
-    const activeTab = tablist.find('.kirk-tab-selected')
-    expect(activeTab.exists()).toBe(true)
-    expect(activeTab.length).toBe(1)
-    expect(activeTab.find('[role="tab"]').prop('aria-selected')).toBe('true')
+    // // Verify the selected tab ARIA state
+    expect(screen.getByRole('tab', { name: 'Tab 1', selected: true })).toBeInTheDocument()
   })
 
   it('Should update ARIA attributes after switching tab', () => {
-    const wrapper = mount(
-      <Tabs tabs={defaultTabsConfig.tabs} activeTabId={defaultTabsConfig.activeTabId} />,
-    )
+    const props = createProps()
+    render(<Tabs {...props} />)
 
     // Verify that initial tab 1 is selected
-    let tabs = wrapper.find('[role="tab"]')
-    expect(tabs.exists()).toBe(true)
-    expect(tabs.length).toBe(3)
+    const tabs = screen.getAllByRole('tab')
     // Verify aria-selected
-    expect(tabs.get(0).props['aria-selected']).toBe('true')
-    expect(tabs.get(1).props['aria-selected']).toBe('false')
-    expect(tabs.get(2).props['aria-selected']).toBe('false')
+    expect(tabs[0]).toHaveAttribute('aria-selected', 'true')
+    expect(tabs[1]).toHaveAttribute('aria-selected', 'false')
+    expect(tabs[2]).toHaveAttribute('aria-selected', 'false')
     // Verify tabIndexes
-    expect(tabs.get(0).props.tabIndex).toBe(0)
-    expect(tabs.get(1).props.tabIndex).toBe(-1)
-    expect(tabs.get(2).props.tabIndex).toBe(-1)
+    expect(tabs[0]).toHaveAttribute('tabindex', '0')
+    expect(tabs[1]).toHaveAttribute('tabindex', '-1')
+    expect(tabs[2]).toHaveAttribute('tabindex', '-1')
 
     // Verify that the current tabpanel id is correct.
-    let tabpanel = wrapper.find('[role="tabpanel"]')
-    expect(tabpanel.get(0).props.id).toBe('tab1_panel')
+    expect(screen.getByRole('tabpanel')).toHaveAttribute('id', 'tab1_panel')
 
     // Simulate a click on tab 2.
-    wrapper
-      .find('[role="tab"]')
-      .at(1)
-      .simulate('click')
+    fireEvent.click(tabs[1])
 
     // Verify that now tab 2 is selected
-    tabs = wrapper.find('[role="tab"]')
     // Verify aria-selected
-    expect(tabs.get(0).props['aria-selected']).toBe('false')
-    expect(tabs.get(1).props['aria-selected']).toBe('true')
-    expect(tabs.get(2).props['aria-selected']).toBe('false')
+    expect(tabs[0]).toHaveAttribute('aria-selected', 'false')
+    expect(tabs[1]).toHaveAttribute('aria-selected', 'true')
+    expect(tabs[2]).toHaveAttribute('aria-selected', 'false')
     // Verify tabIndexes
-    expect(tabs.get(0).props.tabIndex).toBe(-1)
-    expect(tabs.get(1).props.tabIndex).toBe(0)
-    expect(tabs.get(2).props.tabIndex).toBe(-1)
+    expect(tabs[0]).toHaveAttribute('tabindex', '-1')
+    expect(tabs[1]).toHaveAttribute('tabindex', '0')
+    expect(tabs[2]).toHaveAttribute('tabindex', '-1')
 
     // Verify that the current tabpanel id is correct.
-    tabpanel = wrapper.find('[role="tabpanel"]')
-    expect(tabpanel.get(1).props.id).toBe('tab2_panel')
+    expect(screen.getByRole('tabpanel')).toHaveAttribute('id', 'tab2_panel')
   })
 })
 
 describe('Keyboard navigation', () => {
   it('Should left/right keyboard navigate', () => {
-    const wrapper = mount(
-      <Tabs tabs={defaultTabsConfig.tabs} activeTabId={defaultTabsConfig.activeTabId} />,
-    )
+    const props = createProps()
+    render(<Tabs {...props} />)
 
-    let tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(0).props['aria-selected']).toBe('true')
+    screen.getByRole('tab', { name: 'Tab 1', selected: true }).focus()
 
     // Simulate a right arrow keydown.
-    tabs.at(0).simulate('keydown', { key: 'ArrowRight' })
+    fireEvent.keyDown(document.activeElement, { key: 'ArrowRight' })
 
     // Tab 2 is now selected.
-    tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(1).props['aria-selected']).toBe('true')
+    expect(screen.getByRole('tab', { name: 'Tab 2', selected: true })).toHaveFocus()
 
     // Simulate a right arrow keydown.
-    tabs.at(1).simulate('keydown', { key: 'ArrowRight' })
+    fireEvent.keyDown(document.activeElement, { key: 'ArrowRight' })
 
     // Tab 3 is now selected.
-    tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(2).props['aria-selected']).toBe('true')
+    expect(screen.getByRole('tab', { name: 'Tab 3', selected: true })).toHaveFocus()
 
     // Simulate a left arrow keydown.
-    tabs.at(2).simulate('keydown', { key: 'ArrowLeft' })
+    fireEvent.keyDown(document.activeElement, { key: 'ArrowLeft' })
 
     // Tab 2 is now selected.
-    tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(1).props['aria-selected']).toBe('true')
+    expect(screen.getByRole('tab', { name: 'Tab 2', selected: true })).toHaveFocus()
   })
 
-  it('goes to the last tab when going left after the first tab', () => {
-    const wrapper = mount(
-      <Tabs tabs={defaultTabsConfig.tabs} activeTabId={defaultTabsConfig.activeTabId} />,
-    )
+  it('should loop to the last tab when going left starting from the first tab', () => {
+    const props = createProps()
+    render(<Tabs {...props} />)
 
     // Verify that first tab is selected.
-    let tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(0).props['aria-selected']).toBe('true')
+    screen.getByRole('tab', { name: 'Tab 1', selected: true }).focus()
 
     // Simulate a left arrow keydown.
-    tabs.at(0).simulate('keydown', { key: 'ArrowLeft' })
+    fireEvent.keyDown(document.activeElement, { key: 'ArrowLeft' })
 
     // Last tab is now selected.
-    tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(2).props['aria-selected']).toBe('true')
+    expect(screen.getByRole('tab', { name: 'Tab 3', selected: true })).toHaveFocus()
   })
 
-  it('goes to first tab when going right after the last tab', () => {
+  it('should loop to the first tab when going right starting from the last tab', () => {
     // Set-up the last tab to be selected ('tab3')
-    const wrapper = mount(<Tabs tabs={defaultTabsConfig.tabs} activeTabId="tab3" />)
+    const props = createProps({ activeTabId: 'tab3' })
+    render(<Tabs {...props} />)
 
     // Verify that last tab on the right is selected.
-    let tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(2).props['aria-selected']).toBe('true')
+    screen.getByRole('tab', { name: 'Tab 3', selected: true }).focus()
 
     // Simulate a right arrow keydown.
-    tabs.at(2).simulate('keydown', { key: 'ArrowRight' })
+    fireEvent.keyDown(document.activeElement, { key: 'ArrowRight' })
 
     // First tab is now selected.
-    tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(0).props['aria-selected']).toBe('true')
+    expect(screen.getByRole('tab', { name: 'Tab 1', selected: true })).toHaveFocus()
   })
 
   it('Should do nothing when tabbing', () => {
-    const wrapper = mount(
-      <Tabs tabs={defaultTabsConfig.tabs} activeTabId={defaultTabsConfig.activeTabId} />,
-    )
+    const props = createProps()
+    render(<Tabs {...props} />)
 
     // Verify that first tab is selected.
-    let tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(0).props['aria-selected']).toBe('true')
+    screen.getByRole('tab', { name: 'Tab 1', selected: true }).focus()
 
     // Simulate a Tab keydown: The focus will just move outside the tablist and not
     // select another tab in the tablist.
-    tabs.at(0).simulate('keydown', { key: 'Tab' })
+    fireEvent.keyDown(document.activeElement, { key: 'Tab' })
 
     // Verify first tab is still selected.
-    tabs = wrapper.find('[role="tab"]')
-    expect(tabs.get(0).props['aria-selected']).toBe('true')
+    expect(screen.getByRole('tab', { name: 'Tab 1', selected: true })).toBeInTheDocument()
   })
 })
