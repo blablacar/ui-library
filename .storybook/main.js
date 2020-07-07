16ccb837439c1b5ba6c43b6400f2535a43d9a298
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-07-07 10:44:14 +0200

    [BBC-7095] Storybook 5.3+ Config (#779)

---
+++ .storybook/main.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [['react-app', { flow: false, typescript: true }]],
+          },
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+          options: {
+            tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+            shouldExtractLiteralValuesFromEnum: true,
+          },
+        },
+      ],
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+  stories: ['../src/**/story.(tsx|mdx)', '../src/**/*.story.(tsx|mdx)'],
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport',
+    '@storybook/addon-a11y',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import { withSmartKnobs } from 'storybook-addon-smart-knobs'
+import { addDecorator, addParameters } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withA11y } from '@storybook/addon-a11y'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+import '../stories/stories.css'
+
+addParameters({
+  options: {
+    showPanel: true,
+    isToolshown: true,
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+})
+
+addDecorator(withSmartKnobs())
+addDecorator(withKnobs)
+addDecorator(withA11y)
