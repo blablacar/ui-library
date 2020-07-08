2766246ce6f8b05fbef21f295f0db51ed11e4af9
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-07-08 18:40:05 +0200

    [BBC-7096] Disclaimer (#782)

--- src/disclaimer/index.tsx
+++ src/disclaimer/index.tsx
@@ -1,14 +1,1 @@
-import styled from 'styled-components'
-
-import { color } from '../_utils/branding'
-import { Disclaimer } from './Disclaimer'
-
-const StyledDisclaimer = styled(Disclaimer)`
-  .kirk-item {
-    color: ${color.lightMidningGreen};
-  }
-`
-
-export { DisclaimerProps } from './Disclaimer'
-export { StyledDisclaimer as Disclaimer }
-export default StyledDisclaimer
+export { Disclaimer, DisclaimerProps } from './Disclaimer'
