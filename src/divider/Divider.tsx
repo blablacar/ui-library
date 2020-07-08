18d22d6697d3eee7babf74c3e44bdfc1f27b0b2c
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-07-08 18:14:03 +0200

    [BBC-8974] Divider.mdx

--- src/divider/Divider.tsx
+++ src/divider/Divider.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import cc from 'classcat'
+import styled from 'styled-components'
+
+import { color, space } from '../_utils/branding'
 
 export interface DividerProps {
   readonly className?: string
 }
 
+const StyledDivider = styled.div`
+  & {
+    // Height is used instead of margins to prevent collapsing margin issues with other
+    // margin-based components.
+    height: calc(2 * ${space.m});
+    position: relative;
+  }
+
+  &:after {
+    position: absolute;
+    top: ${space.m};
+    content: ' ';
+    border-top: solid ${color.lightGray} 1px;
+    width: 100%;
+  }
+`
+
 export const Divider = ({ className }: DividerProps) => (
-  <div className={cc(className)} aria-hidden="true" />
+  <StyledDivider className={cc(className)} aria-hidden="true" />
 )
