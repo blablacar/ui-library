5492811049dc36583b17aa858bcfd580819d2033
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-07-21 13:52:14 +0200

    Scaffolding component (#796)

---
+++ tools/component/component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Styled__COMPONENT_NAME__ } from './__COMPONENT_NAME__.style'
+
+export interface __COMPONENT_NAME__ {}
+
+export const __COMPONENT_NAME__ = ({}: __COMPONENT_NAME__Props) => {
+  return <Styled__COMPONENT_NAME__> Content </Styled__COMPONENT_NAME__>
+}
---
+++ tools/component/index.tsx
@@ -0,0 +1,2 @@
+export { __COMPONENT_NAME__ } from './__COMPONENT_NAME__'
+export { __COMPONENT_NAME__ as default } from './__COMPONENT_NAME__'
