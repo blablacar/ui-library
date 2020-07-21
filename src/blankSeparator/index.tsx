f30ecf7215522c737597439f3a77c69559e72595
Simon Relet <simon.relet@blablacar.com>
2020-07-21 15:27:40 +0200

    Migrate BlankSeparator to new doc format

--- src/blankSeparator/index.tsx
+++ src/blankSeparator/index.tsx
@@ -1,24 +1,2 @@
-import styled from 'styled-components'
-
-import { space } from '../_utils/branding'
-import { BlankSeparator, BlankSeparatorSize } from './BlankSeparator'
-
-const StyledBlankSeparator = styled(BlankSeparator)`
-  & {
-    /* No background color for blank separator. */
-    padding-top: ${props => {
-      switch (props.size) {
-        case BlankSeparatorSize.LARGE:
-          return space.xl
-        case BlankSeparatorSize.MEDIUM:
-          return space.l
-        case BlankSeparatorSize.SMALL:
-        default:
-          return space.m
-      }
-    }};
-  }
-`
-export { BlankSeparatorSize, BlankSeparatorProps } from './BlankSeparator'
-export { StyledBlankSeparator as BlankSeparator }
-export default StyledBlankSeparator
+export { BlankSeparator, BlankSeparatorSize, BlankSeparatorProps } from './BlankSeparator'
+export { BlankSeparator as default } from './BlankSeparator'
