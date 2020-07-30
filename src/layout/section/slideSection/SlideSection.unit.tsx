91957cb453d96de6188bfe82071f225f3a8285b1
Perrine Boissières <perrine.boissieres@blablacar.com>
2020-07-30 11:20:59 +0200

    rtl

--- src/layout/section/slideSection/SlideSection.unit.tsx
+++ src/layout/section/slideSection/SlideSection.unit.tsx
@@ -1,37 +1,29 @@
 import React from 'react'
-import { mount, shallow } from 'enzyme'
 
 import { render } from '@testing-library/react'
 
-import { Grip } from '../../../grip'
 import { SlideSection } from './index'
 
 describe('SlideSection', () => {
-  it('Should render a Grip component', () => {
-    const container = shallow(
-      <SlideSection media={<div>Media</div>}>{() => <div>Children</div>}</SlideSection>,
-    )
-    expect(container.find(Grip).exists()).toBe(true)
-  })
   it('Should render the elements from media prop', () => {
-    const container = mount(
+    const { getByText } = render(
       <SlideSection media={<div>Media</div>}>{() => <div>Children</div>}</SlideSection>,
     )
-    expect(container.text()).toContain('Media')
+    expect(getByText('Media')).toBeInTheDocument()
   })
   it('Should render the elements from reducedContent prop', () => {
-    const container = mount(
+    const { getByText } = render(
       <SlideSection media={<div>Media</div>} reducedContent={<div>ReducedContent</div>}>
         {() => <div>Children</div>}
       </SlideSection>,
     )
-    expect(container.text()).toContain('ReducedContent')
+    expect(getByText('ReducedContent')).toBeInTheDocument()
   })
   it('Should render its children', () => {
-    const container = mount(
+    const { getByText } = render(
       <SlideSection media={<div>Media</div>}>{() => <div>Children</div>}</SlideSection>,
     )
-    expect(container.text()).toContain('Children')
+    expect(getByText('Children')).toBeInTheDocument()
   })
   it('Should change the position when calling the children methods', () => {
     const { container, getByText } = render(
@@ -52,12 +44,12 @@ describe('SlideSection', () => {
       </SlideSection>,
     )
     getByText('reduce').click()
-    expect(container.querySelector('.reduced')).toMatchSnapshot()
+    expect(container.querySelector('.reduced')).toBeInTheDocument()
 
     getByText('expand').click()
-    expect(container.querySelector('.expanded')).toMatchSnapshot()
+    expect(container.querySelector('.expanded')).toBeInTheDocument()
 
     getByText('default').click()
-    expect(container.querySelector('.default')).toMatchSnapshot()
+    expect(container.querySelector('.default')).toBeInTheDocument()
   })
 })
