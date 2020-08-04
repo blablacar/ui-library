f2a5712d29f01bfef0e88734732687caf28f9f15
Perrine Boissières <perrine.boissieres@blablacar.com>
2020-08-04 16:29:15 +0200

    feedbacks and typo fixes

--- src/itemRadio/ItemRadio.style.tsx
+++ src/itemRadio/ItemRadio.style.tsx
@@ -4,14 +4,13 @@ import { Item } from '../_internals/item'
 import { color, inputBorderSize, space } from '../_utils/branding'
 
 export const StyledItemRadio = styled(Item)`
-  & input {
+  cursor: ${props => (props.disabled ? 'default' : 'pointer')};
+  border: ${inputBorderSize.focus} solid transparent;
+  padding: calc(${space.l} - ${inputBorderSize.focus}) auto;
+
+  input {
     position: absolute;
     clip: rect(0, 0, 0, 0);
-  }
-  & {
-    cursor: ${props => (props.disabled ? 'default' : 'pointer')};
-    border: ${inputBorderSize.focus} solid transparent;
-    padding: calc(${space.l} - ${inputBorderSize.focus}) auto;
   }
   &.focus {
     border: ${inputBorderSize.focus} solid ${color.blue};
