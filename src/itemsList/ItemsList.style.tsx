1f00f4519bcbf8bedc6f4f991f2a274c38e6f9b0
Perrine Boissières <perrine.boissieres@blablacar.com>
2020-08-04 15:44:13 +0200

    ItemsList with new docs

---
+++ src/itemsList/ItemsList.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledItemsList = styled.ul`
+  & {
+    display: flex;
+    flex-direction: column;
+    list-style-type: none;
+  }
+`
---
+++ src/itemsList/index.ts
@@ -0,0 +1,2 @@
+export { ItemsList, ItemsListProps, ItemsListDivider, ItemsListChild } from './ItemsList'
+export { ItemsList as default } from './ItemsList'
