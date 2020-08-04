10aaaa6bc18b2ab41e20bdea2044d4ae6dd8f75d
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-08-04 17:41:22 +0200

    [BBC-9048] ItemInfo.MDX (#836)

--- src/itemInfo/ItemInfo.unit.tsx
+++ src/itemInfo/ItemInfo.unit.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import { ItemInfo } from '../itemInfo'
+import { ItemInfo } from './index'
 
 const defaultProps = {
   mainInfo: 'Main information',
