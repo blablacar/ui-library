3595ab6c524e3f8f95d5faa8a2493c0341d0eb54
Jérôme Smadja <jerome.smadja@blablacar.com>
2020-08-07 17:40:58 +0200

    Revert "Hero Section update cleaning/removing of non used props, and changed layout (#843)"
    This reverts commit 8cf8716f1ab5ea4efd5770b7d8919dac5c900f4d.

--- src/layout/section/heroSection/heroSection.tsx
+++ src/layout/section/heroSection/heroSection.tsx
@@ -1,34 +1,49 @@
 import React from 'react'
 
-import {
-  HeroContent,
-  HeroImage,
-  HeroInfo,
-  HeroTextDisplay1,
-  HeroTitle,
-  HeroTitleWrapper,
-  StyledHeroSection,
-} from '.'
+import { Button, ButtonProps } from '../../../button'
+import { TextDisplay1 } from '../../../typography/display1'
+import { TextTitle } from '../../../typography/title'
 
 export type HeroSectionProps = {
-  heroImageUrl: string
+  className?: string
+  heroImageUrl?: string
+  heroImageUrlLarge: string
   heroText?: string
-  bottomElement: React.ReactElement
+  heroDescription?: string
+  buttonText?: string
+  buttonHref?: ButtonProps['href']
+  bottomElement?: React.ReactElement
 }
 
-export function HeroSection({ heroText, bottomElement, heroImageUrl }: HeroSectionProps) {
+export function HeroSection({
+  className,
+  heroText,
+  heroDescription,
+  buttonText,
+  buttonHref,
+  bottomElement,
+}: HeroSectionProps) {
   return (
-    <StyledHeroSection>
-      <HeroImage heroImageUrl={heroImageUrl} />
-      <HeroInfo>
-        <HeroTitleWrapper>
-          <HeroTitle>
-            <HeroTextDisplay1>{heroText}</HeroTextDisplay1>
-          </HeroTitle>
-        </HeroTitleWrapper>
+    <div className={className}>
+      <div className="hero-image" aria-hidden="true" />
 
-        <HeroContent>{bottomElement}</HeroContent>
-      </HeroInfo>
-    </StyledHeroSection>
+      <div className="hero-info">
+        <div className="hero-info-title-wrapper">
+          <h1 className="hero-info-title">
+            <TextDisplay1 className="hero-info-title--display1">{heroText}</TextDisplay1>
+          </h1>
+
+          {heroDescription && (
+            <p className="hero-info-text">
+              <TextTitle className="hero-info-text-title">{heroDescription}</TextTitle>
+            </p>
+          )}
+        </div>
+
+        <div className="hero-content">
+          {bottomElement ?? <Button href={buttonHref}>{buttonText}</Button>}
+        </div>
+      </div>
+    </div>
   )
 }
