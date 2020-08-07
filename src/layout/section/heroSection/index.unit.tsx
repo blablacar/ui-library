8cf8716f1ab5ea4efd5770b7d8919dac5c900f4d
Jerome Smadja <wakooka@users.noreply.github.com>
2020-08-07 17:34:54 +0200

    Hero Section update cleaning/removing of non used props, and changed layout (#843)
    Co-authored-by: Jérôme Smadja <jerome.smadja@blablacar.com>

--- src/layout/section/heroSection/index.unit.tsx
+++ src/layout/section/heroSection/index.unit.tsx
@@ -7,33 +7,19 @@ import { HeroSection, HeroSectionProps } from './heroSection'
 function createProps(props: Partial<HeroSectionProps>): HeroSectionProps {
   return {
     heroText: 'hero text',
-    heroDescription: 'hero description',
-    heroImageUrlLarge: 'http://heroImageUrl',
+    heroImageUrl: 'http://heroImageUrl.small',
+    bottomElement: <div data-testid="bottom-element">Bottom element</div>,
     ...props,
   }
 }
 
 describe('HeroSection', () => {
   it('should render basic hero section', () => {
-    const props = createProps({
-      heroImageUrl: 'http://heroImageUrl',
-      buttonText: 'button text',
-      buttonHref: 'http://buttonhref',
-    })
+    const props = createProps({})
 
-    render(<HeroSection {...props} />)
-
-    expect(screen.getByText(props.heroText)).toBeInTheDocument()
-    expect(screen.getByText(props.heroDescription)).toBeInTheDocument()
-    expect(screen.getByText(props.buttonText)).toHaveAttribute('href', props.buttonHref)
-  })
-
-  it('should render a search form', () => {
-    const props = createProps({
-      bottomElement: <div data-testid="bottom-element">Bottom element</div>,
-    })
-
-    render(<HeroSection {...props} />)
+    const { container } = render(<HeroSection {...props} />)
+    // Not using `getByRole('heading')` to make sure that the tag used is h1 for SEO purposes
+    expect(container.querySelector('h1')).toHaveTextContent(props.heroText)
     expect(screen.getByTestId(props.bottomElement.props['data-testid'])).toBeInTheDocument()
   })
 })
