a4e1cd3f38f1104a4fa25b739f68f754f00d0da7
nickoola <nicolas.esneault@blablacar.com>
2020-08-26 17:46:28 +0200

    Display itemCheckbox focus ring on keyboard nav

--- src/itemCheckbox/ItemCheckbox.style.tsx
+++ src/itemCheckbox/ItemCheckbox.style.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
 
 import { Item } from '../_internals/item'
+import { color, inputBorderSize } from '../_utils/branding'
 
 export const StyledItemCheckbox = styled(Item)`
   cursor: ${props => (props.disabled ? 'default' : 'pointer')};
+
+  &.focus-visible {
+    outline: ${inputBorderSize.focus} solid ${color.blue};
+  }
 
   & input {
     position: absolute;
