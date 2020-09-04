0234339da1f4a808f9b022f4f5904b6e1fa501e5
Michael Cellier <michael.cellier@gmail.com>
2020-09-04 11:54:51 +0200

    Remove style specificity

--- src/layout/section/mediaSection/MediaSection.style.tsx
+++ src/layout/section/mediaSection/MediaSection.style.tsx
@@ -4,12 +4,10 @@ import { componentSizes, responsiveBreakpoints, space } from '../../../_utils/br
 
 export const StyledMediaSection = styled.div`
   @media (${responsiveBreakpoints.isMediaLarge}) {
-    & {
-      padding-left: ${space.xl};
-      padding-right: ${space.xl};
-      margin-left: auto;
-      margin-right: auto;
-      max-width: ${componentSizes.smallSectionWidth};
-    }
+    padding-left: ${space.xl};
+    padding-right: ${space.xl};
+    margin-left: auto;
+    margin-right: auto;
+    max-width: ${componentSizes.smallSectionWidth};
   }
 `
