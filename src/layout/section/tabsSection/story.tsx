025cfb58c56b29b7ba648cc21141b8c82f090f92
Simon Relet <simon.relet@blablacar.com>
2020-09-17 16:35:02 +0200

    Only use knobs in stories

--- src/layout/section/tabsSection/story.tsx
+++ src/layout/section/tabsSection/story.tsx
@@ -28,30 +28,32 @@ const panels = [
   </BaseSection>,
 ]
 
-const tabs = {
-  activeTabId: 'tab1',
-  status: select('status', TabStatus, TabStatus.FIXED),
-  tabs: [
-    {
-      id: 'tab1',
-      label: text('Tab label 1', 'Tab 1'),
-      panelContent: panels[0],
-      badgeContent: text('Badge content 1', ''),
-    },
-    {
-      id: 'tab2',
-      label: text('Tab label 2', 'Very Very Long Tab 2'),
-      panelContent: panels[1],
-      badgeContent: text('Badge content 2', '2'),
-      badgeAriaLabel: 'Unread Message',
-    },
-    {
-      id: 'tab3',
-      label: text('Tab label 3', 'Tab 3'),
-      panelContent: panels[2],
-      badgeContent: text('Badge content 3', ''),
-    },
-  ],
-}
-
-stories.add('default', () => <TabsSection tabsProps={tabs} />)
+stories.add('default', () => (
+  <TabsSection
+    tabsProps={{
+      activeTabId: 'tab1',
+      status: select('status', TabStatus, TabStatus.FIXED),
+      tabs: [
+        {
+          id: 'tab1',
+          label: text('Tab label 1', 'Tab 1'),
+          panelContent: panels[0],
+          badgeContent: text('Badge content 1', ''),
+        },
+        {
+          id: 'tab2',
+          label: text('Tab label 2', 'Very Very Long Tab 2'),
+          panelContent: panels[1],
+          badgeContent: text('Badge content 2', '2'),
+          badgeAriaLabel: 'Unread Message',
+        },
+        {
+          id: 'tab3',
+          label: text('Tab label 3', 'Tab 3'),
+          panelContent: panels[2],
+          badgeContent: text('Badge content 3', ''),
+        },
+      ],
+    }}
+  />
+))
