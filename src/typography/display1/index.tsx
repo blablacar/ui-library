f0fbd7ef6bf06df2cef9783768a8efa30182a9c3
Michael Cellier <michael.cellier@blablacar.com>
2020-11-02 15:02:17 +0100

    Migrate typography components to MDX

--- src/typography/display1/index.tsx
+++ src/typography/display1/index.tsx
@@ -1,11 +1,1 @@
-import styled from 'styled-components'
-
-import { color, font, fontWeight } from '../../_utils/branding'
-import { Text } from '../index'
-
-export const TextDisplay1 = styled(Text)`
-  color: ${color.midnightGreen};
-  font-size: ${font.xl.size};
-  line-height: ${font.xl.lineHeight};
-  font-weight: ${fontWeight.medium};
-`
+export { TextDisplay1 } from './Display1.style'
