496a5fc8141277a17a5ed4b9c5558eb2955b212a
Michael Cellier <michael.cellier@gmail.com>
2020-11-02 13:48:11 +0100

    [BOYSCOUT] Migrate `Bullet` to TSX (#899)

--- src/bullet/Bullet.unit.tsx
+++ src/bullet/Bullet.unit.tsx
@@ -3,12 +3,14 @@ import { shallow } from 'enzyme'
 
 import { Bullet, BulletTypes } from './Bullet'
 
-it('Should render a Bullet with "default" type if not specified', () => {
-  const wrapper = shallow(<Bullet />)
-  expect(wrapper.find('div').hasClass('kirk-bullet--default')).toBe(true)
-})
+describe('Bullet', () => {
+  it('Should render a Bullet with "default" type if not specified', () => {
+    const wrapper = shallow(<Bullet />)
+    expect(wrapper.hasClass('kirk-bullet--default')).toBe(true)
+  })
 
-it('Should render a Bullet with "addon" type', () => {
-  const wrapper = shallow(<Bullet type={BulletTypes.ADDON} />)
-  expect(wrapper.find('div').hasClass('kirk-bullet--addon')).toBe(true)
+  it('Should render a Bullet with "addon" type', () => {
+    const wrapper = shallow(<Bullet type={BulletTypes.ADDON} />)
+    expect(wrapper.hasClass('kirk-bullet--addon')).toBe(true)
+  })
 })
