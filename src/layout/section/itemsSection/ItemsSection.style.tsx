b4be6da5f1e05643e3922bef59abe626e4020e8f
Michael Cellier <michael.cellier@blablacar.com>
2020-11-03 11:08:39 +0100

    [BBC-9124] Migrate ItemsSection to MDX

---
+++ src/layout/section/itemsSection/ItemsSection.style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import { BaseSection } from '../../../layout/section/baseSection'
+
+export const StyledItemsSection = styled(BaseSection)`
+  & .kirk-items-section-content {
+    display: flex;
+  }
+`
