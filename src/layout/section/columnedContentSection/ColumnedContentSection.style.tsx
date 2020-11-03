beeb4fcb254a3499424f590df8dde97a29439a45
Michael Cellier <michael.cellier@blablacar.com>
2020-11-03 10:41:08 +0100

    Migrate ColumnedContentSection to MDX

---
+++ src/layout/section/columnedContentSection/ColumnedContentSection.style.tsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+
+import { font, radius, responsiveBreakpoints, space } from '../../../_utils/branding'
+import { BaseSection } from '../baseSection'
+
+export const StyledColumnedContentSection = styled(BaseSection)`
+  & {
+    margin: ${space.xl} 0;
+  }
+
+  & .kirk-columned-content-section-title {
+    padding: ${space.xl} 0;
+    margin: 0;
+  }
+
+  & .kirk-columned-content-section-content {
+    position: relative;
+  }
+
+  & .kirk-columned-content-section-top-link {
+    position: absolute;
+    top: ${space.s};
+    right: 0;
+  }
+
+  & .kirk-columned-content-section-subtitle {
+    font-size: ${font.m.size};
+    line-height: ${font.m.lineHeight};
+    padding-top: ${space.m};
+    padding-bottom: ${space.s};
+    margin: 0;
+  }
+
+  & .kirk-columned-content-section-footer-link {
+    font-size: ${font.m.size};
+    line-height: ${font.m.lineHeight};
+    padding-top: ${space.m};
+  }
+
+  & .kirk-columned-content-section-media-cover img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: ${radius.s};
+  }
+
+  & .kirk-columned-content-section-media-element {
+    text-align: center;
+  }
+
+  & .kirk-columned-content-section-media-fit {
+    text-align: center;
+  }
+
+  & .kirk-columned-content-section-media-fit img {
+    width: 60%;
+    border-radius: ${radius.s};
+  }
+
+  @media (${responsiveBreakpoints.isMediaSmall}) {
+    & .kirk-columned-content-section-title {
+      text-align: center;
+    }
+
+    & .kirk-columned-content-section-top-link {
+      display: none;
+    }
+
+    & .kirk-columned-content-section-column:not(:first-child):not(:last-child) {
+      margin-top: ${space.xl};
+      margin-bottom: ${space.xl};
+    }
+
+    & .kirk-columned-content-section-column {
+      text-align: center;
+    }
+  }
+
+  @media (${responsiveBreakpoints.isMediaLarge}) {
+    & .kirk-columned-content-section-column:not(:first-child):not(:last-child) {
+      margin-left: ${space.xxl};
+      margin-right: ${space.xxl};
+    }
+  }
+`
