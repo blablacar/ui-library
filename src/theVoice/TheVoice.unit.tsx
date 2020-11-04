f8148124ec12072453a179a00eafbd1abde38249
Sylvain H <Tip-Sy@users.noreply.github.com>
2020-11-04 10:31:21 +0100

    [BBC-9088] Migrate TheVoice to MDX (#804)
    Co-authored-by: Michael Cellier <michael.cellier@blablacar.com>

--- src/theVoice/TheVoice.unit.tsx
+++ src/theVoice/TheVoice.unit.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import { mount, shallow } from 'enzyme'
 
-import { Title } from '../title'
+import { render, screen } from '@testing-library/react'
+
 import { TheVoice } from './TheVoice'
 
 describe('TheVoice', () => {
+  const title = 'the voice content'
+
   it('Should use a h1 with the voice content', () => {
-    const title = 'the voice content'
-    const wrapper = mount(<TheVoice>{title}</TheVoice>)
-    const heading = wrapper.find('h1')
-    expect(heading.exists()).toBe(true)
-    expect(heading.text()).toBe('the voice content')
+    render(<TheVoice>{title}</TheVoice>)
+    expect(screen.getByRole('heading', { level: 1, name: title }))
   })
 
   it('Should forward id to Title if provided', () => {
-    const wrapper = shallow(<TheVoice id="my-id">My title</TheVoice>)
-    expect(wrapper.find(Title).exists()).toBe(true)
-    expect(wrapper.find(Title).prop('id')).toBe('my-id')
+    render(<TheVoice id="my-id">{title}</TheVoice>)
+    expect(screen.getByRole('heading')).toHaveAttribute('id', 'my-id')
   })
 })
