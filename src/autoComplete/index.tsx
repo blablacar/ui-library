62767bf167db51f09945429775b571a27f495a60
Pierre Thirouin <pierre.thirouin@blablacar.com>
2020-11-04 09:44:07 +0100

    Migrate Autocomplete to MDX (#948)

--- src/autoComplete/index.tsx
+++ src/autoComplete/index.tsx
@@ -1,41 +1,7 @@
-import styled from 'styled-components'
-
-import { AutoComplete } from './AutoComplete'
-
-const StyledAutoComplete = styled(AutoComplete)`
-  & {
-    display: flex;
-    flex-direction: column;
-  }
-
-  & .kirk-autoComplete-body {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex: 1;
-    min-height: 300px;
-  }
-
-  /**
-   * Address appearance set to searchfield in Safari and Chrome.
-   * Address box-sizing set to border-box in Safari and Chrome
-   */
-  & input[type='search'] {
-    -webkit-appearance: textfield;
-    box-sizing: content-box;
-  }
-
-  /**
-   * Remove inner padding and search cancel button in Safari and Chrome on OS X.
-   * Safari (but not Chrome) clips the cancel button when the search input has
-   * padding (and textfield appearance).
-   */
-  & input[type='search']::-webkit-search-cancel-button,
-  & input[type='search']::-webkit-search-decoration {
-    -webkit-appearance: none;
-  }
-`
-
-export * from './AutoComplete'
-export { StyledAutoComplete as AutoComplete }
-export default StyledAutoComplete
+export {
+  AutoComplete,
+  AutoCompleteProps,
+  AutocompleteOnChange,
+  AutocompleteItem,
+} from './AutoComplete'
+export { AutoComplete as default } from './AutoComplete'
