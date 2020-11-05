87f5b93d2bcdec5ea6d53edc3d6d1c54a1434863
Michael Cellier <michael.cellier@blablacar.com>
2020-11-05 15:17:03 +0100

    PR feedback

--- src/toggleButton/ToggleButton.style.tsx
+++ src/toggleButton/ToggleButton.style.tsx
@@ -1,19 +1,17 @@
 import styled from 'styled-components'
 
 export const StyledToggleButton = styled.button`
-  & {
-    display: block;
-    margin: 0;
-    padding: 0;
-    background: none;
-    width: 100%;
-    border: 0;
-    text-align: left;
-    font-family: inherit;
-    cursor: pointer;
-    user-select: none;
-    overflow: hidden;
-  }
+  display: block;
+  margin: 0;
+  padding: 0;
+  background: none;
+  width: 100%;
+  border: 0;
+  text-align: left;
+  font-family: inherit;
+  cursor: pointer;
+  user-select: none;
+  overflow: hidden;
 
   &:active {
     outline: 0;
--- src/toggleButton/ToggleButton.unit.tsx
+++ src/toggleButton/ToggleButton.unit.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import { ToggleButton, ToggleButtonStatus } from './ToggleButton'
+import { ToggleButton, ToggleButtonStatus } from './index'
 
 const defaultProps = {
   name: 'foo',
