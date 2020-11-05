6caf9d56c9094a49812d62bf550f92db384161a6
Michael Cellier <michael.cellier@blablacar.com>
2020-11-05 17:10:44 +0100

    [BBC-7096] Storybook: toggleButton mdx (#972)

--- src/toggleButton/index.tsx
+++ src/toggleButton/index.tsx
@@ -1,26 +1,2 @@
-import styled from 'styled-components'
-
-import { ToggleButton } from './ToggleButton'
-
-const StyledToggleButton = styled(ToggleButton)`
-  & {
-    display: block;
-    margin: 0;
-    padding: 0;
-    background: none;
-    width: 100%;
-    border: 0;
-    text-align: left;
-    font-family: inherit;
-    cursor: pointer;
-    user-select: none;
-    overflow: hidden;
-  }
-
-  &:active {
-    outline: 0;
-  }
-`
-export { ToggleButtonProps, ToggleButtonStatus } from './ToggleButton'
-export { StyledToggleButton as ToggleButton }
-export default StyledToggleButton
+export { ToggleButton, ToggleButtonProps, ToggleButtonStatus } from './ToggleButton'
+export { ToggleButton as default } from './ToggleButton'
