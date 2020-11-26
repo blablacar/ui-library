0bb8442e67c84d60e591c6c9aec66fc09ac1b4a8
Cyrille Jouineau <cyrille.jouineau@blablacar.com>
2020-11-26 12:25:06 +0100

    add prop divider with DividerPosition enum to Breadcrumb (#998)

--- src/breadcrumb/Breadcrumb.style.tsx
+++ src/breadcrumb/Breadcrumb.style.tsx
@@ -1,9 +1,22 @@
 import styled from 'styled-components'
 
 import { color, space } from '../_utils/branding'
+import { DividerPosition } from './Breadcrumb'
 
-export const StyledBreadcrumb = styled.ol`
-  padding: ${space.l} ${space.xl} ${space.m};
+export const StyledBreadcrumb = styled.ol<{
+  divider: DividerPosition
+}>`
+  padding: ${props => {
+    if (props.divider === DividerPosition.TOP) {
+      return `${space.m} ${space.xl} ${space.l}`
+    }
+    if (props.divider === DividerPosition.BOTTOM) {
+      return `${space.l} ${space.xl} ${space.m}`
+    }
+
+    return `${space.l} ${space.xl} ${space.l}`
+  }};
+
   margin: 0;
   list-style: none;
 
