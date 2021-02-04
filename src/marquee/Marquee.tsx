bb2409eec740e4ec3138babafd8ad5572c2e40a8
Cyrille Jouineau <cyrille.jouineau@blablacar.com>
2021-02-04 16:07:26 +0100

    update api name and style for height (#1077)

--- src/marquee/Marquee.tsx
+++ src/marquee/Marquee.tsx
@@ -3,19 +3,20 @@ import React from 'react'
 import { StyledMarquee, StyledMarqueeItem } from './Marquee.style'
 
 export type MarqueeProps = Readonly<{
+  className?: string
   children: Array<React.ReactNode>
 }>
 
 export const Marquee = (props: MarqueeProps): JSX.Element => {
-  const { children } = props
+  const { className, children } = props
 
   return (
-    <StyledMarquee>
+    <StyledMarquee className={className}>
       {children.filter(Boolean).map((child, index) => {
         const key = `marquee-item-${index}`
 
         return (
-          <StyledMarqueeItem delay={index} totalItems={children.length} key={key}>
+          <StyledMarqueeItem position={index} totalItems={children.length} key={key}>
             {child}
           </StyledMarqueeItem>
         )
