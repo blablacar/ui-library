f90a02e64b9360d91b8536e77a0a301913e1434b
Gabriela Gabriel <gabrielagabriel@gmail.com>
2021-02-23 14:26:48 +0100

    [PROPIN-558] Brand New Highlight Section (#1070)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,11 +2,11 @@
 
 [...]
 
+- **[BREAKING CHANGE]** Brand new `HighlightSection` component.
 # v47.6.1 (22/02/2021)
 
 - **[FIX]** `Icons` display on Storybook Canvas
 - **[FIX]** `SelectField` and `PhoneField` unclickable down arrow
-
 # v47.6.0 (17/02/2021)
 
 - **[NEW]** Added `display` prop to `Proximity`
--- src/layout/section/highlightSection/HighlightSection.tsx
+++ src/layout/section/highlightSection/HighlightSection.tsx
@@ -1,27 +1,102 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import uniqueId from 'lodash.uniqueid'
 
-import { SectionContentSize } from '../../../layout/section/baseSection'
-import { StyledHighlightSection } from './HighlightSection.style'
+import { ItemChoiceProps } from '../../../itemChoice'
+import { Col, Grid, HighlightSectionElements } from './HighlightSection.style'
 
+export type HighlightItemsType = ItemChoiceProps &
+  Readonly<{
+    hidden?: boolean
+  }>
+
+type GridListItemsProps = { items: Array<HighlightItemsType> }
+const GridListItems = ({ items }: GridListItemsProps) => {
+  const listItems = items.map(({ id, label, data, hidden, href }) => (
+    <Col key={id} hidden={hidden}>
+      <HighlightSectionElements.Item label={label} data={data} href={href} />
+    </Col>
+  ))
+  return <Grid>{listItems}</Grid>
+}
+
+type HighlightContentItemsProps = {
+  heading: string
+  items: Array<HighlightItemsType>
+}
+const HighlightContentItems = ({ heading, items }: HighlightContentItemsProps) => (
+  <HighlightSectionElements.Article>
+    <HighlightSectionElements.Title as="h2">{heading}</HighlightSectionElements.Title>
+    <GridListItems items={items} />
+  </HighlightSectionElements.Article>
+)
+
+const DEFAULT_ITEMS_SIZE = 3
+
+export type HighlightsType = { heading: string; items: Array<HighlightItemsType> }
 export type HighlightSectionProps = Readonly<{
   className?: string
-  contentClassName?: string
-  children: React.ReactNode
+  highlights: { axes: HighlightsType; destinations?: HighlightsType }
+  toggle: { on: string; off: string }
 }>
 
-/**
- * A specialized section with an highlighting background color.
- */
-export const HighlightSection = (props: HighlightSectionProps) => {
-  const { className, children, contentClassName } = props
+const setFocus = (collapsed: Boolean) => {
+  const collapsibleRegionWrapper = useRef()
+
+  const focusCollapsibleRegion = (): void => {
+    const collapsibleRegion = collapsibleRegionWrapper.current as HTMLDivElement
+    collapsibleRegion.focus()
+  }
+
+  useEffect(() => {
+    if (!collapsed) {
+      focusCollapsibleRegion()
+    }
+  }, [collapsed])
+
+  return collapsibleRegionWrapper
+}
+
+export const HighlightSection = ({ highlights, toggle, className }: HighlightSectionProps) => {
+  const [collapsed, setCollapsed] = useState(true)
+  const { axes, destinations } = highlights
+
+  const displayedItems = axes.items.map((item, index) => ({
+    ...item,
+    hidden: collapsed && index >= DEFAULT_ITEMS_SIZE,
+  }))
+
+  const collapsibleRegionId = uniqueId('region-')
+
+  // Set Focus
+
+  const collapsibleRegionWrapper = destinations ? setFocus(collapsed) : null
+
   return (
-    <StyledHighlightSection
-      tagName="article"
-      className={className}
-      contentClassName={contentClassName}
-      contentSize={SectionContentSize.LARGE}
-    >
-      {children}
-    </StyledHighlightSection>
+    <HighlightSectionElements.Section className={className}>
+      <HighlightSectionElements.Content>
+        <HighlightContentItems heading={axes.heading} items={displayedItems} />
+        {destinations && (
+          <div
+            id={collapsibleRegionId}
+            ref={collapsibleRegionWrapper}
+            hidden={collapsed}
+            aria-hidden={collapsed}
+            role="region"
+            tabIndex={-1}
+          >
+            <HighlightContentItems heading={destinations.heading} items={destinations.items} />
+          </div>
+        )}
+        <HighlightSectionElements.Actions>
+          <HighlightSectionElements.Button
+            onClick={() => setCollapsed(!collapsed)}
+            aria-expanded={!collapsed}
+            aria-controls={collapsibleRegionId}
+          >
+            {toggle[collapsed ? 'on' : 'off']}
+          </HighlightSectionElements.Button>
+        </HighlightSectionElements.Actions>
+      </HighlightSectionElements.Content>
+    </HighlightSectionElements.Section>
   )
 }
